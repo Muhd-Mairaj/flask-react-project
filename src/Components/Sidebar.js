cba3587a1fee2c5a99b2c6eb4153d3b0da4f3f0b
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-24 23:19:27 +0000

    Sat, Dec 24, 2022, 11:19 PM +00:00

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -5,7 +5,7 @@ import { NavLink, Link } from 'react-router-dom';
 function Sidebar() {
   return (
     <Navbar sticky="top" expand="md" bg="dark" className="flex-row Sidebar nav">
-      <Navbar.Brand to="">Tracker</Navbar.Brand>
+      <Navbar.Brand to="/home">Tracker</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
