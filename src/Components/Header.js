1f19b169ef359991654632a17fa0f8d274279b3e
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 04:48:52 +0000

    Sun, Dec 25, 2022, 4:48 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ import '../index.css'
 function Header() {
   return (
     <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark" className="Header">
-      <Container fluid>
+      <Container>
         <Navbar.Brand href="/">Tracker</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
