aecaaf92c8851a6607a10c258ae9ac10cc8b2ce3
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 00:17:07 +0000

    Sun, Dec 25, 2022, 12:17 AM +00:00

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,6 +1,6 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 import { NavLink } from 'react-router-dom';
 import '../index.css'
 
