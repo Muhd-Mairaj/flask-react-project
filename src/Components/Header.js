da0e25cf3b09516221ac9f550269982e2c543f94
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 06:25:31 +0000

    Sun, Dec 25, 2022, 6:25 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -18,7 +18,7 @@ function Header() {
             <Nav.Link as={NavLink} to="/test">Test</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link as=[NavLink] to="/" href="#deets">More deets</Nav.Link>
+            <Nav.Link as=[NavLink] to="/logout" href="#deets">More deets</Nav.Link>
             <Nav.Link as=[NavLink] to="/" eventKey={2} href="#memes">
               Dank memes
             </Nav.Link>
