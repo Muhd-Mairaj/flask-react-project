a0c9f6eb73e9a41fb6a14d29593d2cd9c24e5039
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 21:42:34 +0000

    Sun, Dec 25, 2022, 9:42 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,7 +32,7 @@ function Home() {
       <Container fluid className="Home">
 
         <Row>
-          Col
+          <Col 
         </Row>
         {/* <Stack direction="horizontal" className="horizontalForm" gap={3}>
           // <Form onSubmit={handleSubmit}>
