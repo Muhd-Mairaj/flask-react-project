6a562dcd9c8d346ff918b5034278f3f2076dee5b
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 22:52:40 +0000

    Sun, Dec 25, 2022, 10:52 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,7 +37,7 @@ function Home() {
       <Form onSubmit={handleSubmit}>
         <Row className="horizontalForm">
           <Col xs={12} sm={7} className="py-1">
-            <Form.Group controlId=item
+            <Form.Group controlId="itemField"
             <Form.Control placeholder="Item name" ref={itemField}/>
           </Col>
           <Col xs={12} sm={3} className="py-1">
