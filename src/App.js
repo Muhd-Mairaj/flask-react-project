7e5c998eab481a30e4e220a9a00b1b5cc2c20809
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 07:16:01 +0000

    Sun, Dec 25, 2022, 7:16 AM +00:00

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container'
 import Header from './Components/Header'
