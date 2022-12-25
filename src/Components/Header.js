cc99416551c73a8d06d3010cd48ccb9aa87d73d9
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 07:19:26 +0000

    Sun, Dec 25, 2022, 7:19 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -29,8 +29,7 @@ function Header({ loggedIn }) {
         <Navbar.Brand href="/">Tracker</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          {
-            loggedIn &&
+          {loggedIn &&
           }
           <Nav className="me-auto">
               <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
