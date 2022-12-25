84fd6125bf54a4987e2be5cf7a747c4cecb9cc69
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 19:42:40 +0000

    Sun, Dec 25, 2022, 7:42 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,17 +7,6 @@ import '../index.css';
 function Home() {
   const [profileData, setProfileData] = useState(null)
 
-    /* METHOD 2 - WORKS */
-// function getData() {
-//   fetch("/profile").then(response => response.json()).then(data => {
-//         console.log(data.items)
-//         setProfileData({
-//             items: data.items,
-//         })
-//       }).catch(error => console.log(error))
-//     }
-    /** END METHOD 2 */
-
   return (
     <Body loggedIn>
       <button onClick={getData}>Get Data</button>
