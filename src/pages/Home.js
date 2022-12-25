26c91ff67728edf60b94e7ffd565b98c4720090b
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 20:27:28 +0000

    Sun, Dec 25, 2022, 8:27 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,8 +30,8 @@ function Home() {
             {Array.from({ length: profileData.items.length }).map((_, index) => (
               <tr>
                 <td>{index + 1}</td>
-                <td  className="bg-danger">{profileData.items[index].item}</td>
-                <td >{profileData.items[index].expiry}</td>
+                <td key={index + 1} className="bg-danger" : ""}>{profileData.items[index].item}</td>
+                <td key={index + 1}>{profileData.items[index].expiry}</td>
               </tr>
             ))}
           </tbody>
