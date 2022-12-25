c6fb5d4b08d19e230d141c6d30310714af48255e
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 07:36:43 +0000

    Sun, Dec 25, 2022, 7:36 AM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,7 +19,7 @@ function getData() {
     /** END METHOD 2 */
 
   return (
-    <Container>
+    <Body>
       <button onClick={getData}>Get Data</button>
             {profileData && <div>
                   <p>Profile name: {profileData.name}</p>
