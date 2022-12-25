a3947b1caccdd2a6d3737d1fcc9c759834f87af4
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 21:30:41 +0000

    Sun, Dec 25, 2022, 9:30 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,7 +31,7 @@ function Home() {
 
         <Stack direction="horizontal" className="horizontalForm" gap={3}>
           {/* <Form onSubmit={handleSubmit}> */}
-            <Form.Control className="me-auto" placeholder="Item name"/>
+            <Form.Control className="me-auto w-auto" placeholder="Item name"/>
             <Form.Control placeholder="Item name" type="date"/>
             <Button variant="dark" className= type="submit">Add</Button>
 
