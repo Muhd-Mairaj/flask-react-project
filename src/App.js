ea8f7413fba0a03ba9857f2e7410ab632eae2e95
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 07:29:54 +0000

    Sun, Dec 25, 2022, 7:29 AM +00:00

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Header from './components/Header';
-import Home from './components/Home';
+import Home from './pages/Home';
 import Test from './components/Test';
 // import logo from './logo.svg';
 // import './App.css';
