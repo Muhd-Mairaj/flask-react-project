223cfb0ad15f929856e3f720407818567868ec88
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 22:39:14 +0000

    Sun, Dec 25, 2022, 10:39 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,7 +34,7 @@ function Home() {
       <Form onSubmit={handleSubmit}>
         <Row className="horizontalForm">
           <Col xs={12} sm={7} className="py-1">
-            <Form.Control className="" placeholder="Item name"/>
+            <Form.Control placeholder="Item name"/>
           </Col>
           <Col xs={12} sm={3} className="py-1">
             <Form.Control className="" placeholder="Item name" type="date"/>
