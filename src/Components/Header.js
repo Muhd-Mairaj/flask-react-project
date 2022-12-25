60d20dec0e1c4cbfec3b67d67b23643d25c5b288
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 07:19:44 +0000

    Sun, Dec 25, 2022, 7:19 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -30,7 +30,6 @@ function Header({ loggedIn }) {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           {loggedIn ?
-          }
           <Nav className="me-auto">
               <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
               <Nav.Link as={NavLink} to="/test">Test</Nav.Link>
@@ -38,6 +37,7 @@ function Header({ loggedIn }) {
           <Nav>
               <Nav.Link as={NavLink} to="/logout" end>Logout</Nav.Link>
           </Nav>
+          }
         </Navbar.Collapse>
       </Container>
     </Navbar>
