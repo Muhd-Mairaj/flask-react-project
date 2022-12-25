ecdeffa26b010e017c04b768170f7856276b7478
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 07:17:27 +0000

    Sun, Dec 25, 2022, 7:17 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -29,6 +29,7 @@ function Header() {
         <Navbar.Brand href="/">Tracker</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
+          {}
           <Nav className="me-auto">
               <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
               <Nav.Link as={NavLink} to="/test">Test</Nav.Link>
