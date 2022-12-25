4574972b6dce8c19ba3fb4b238dc62e3efe5d2f9
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 20:06:08 +0000

    Sun, Dec 25, 2022, 8:06 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,7 +18,7 @@ function Home() {
   
   return (
     <Body loggedIn>
-      {profileData && <Table striped bordered variant="dark" responsive="sm">
+      {profileData && <Table striped bordered variant="dark" responsive="lg">
           <thead>
             <tr>
               <th>#</th>
