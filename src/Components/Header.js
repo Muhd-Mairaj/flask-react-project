688ce580058614e9baa70ea4016441c823f7d166
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 06:22:38 +0000

    Sun, Dec 25, 2022, 6:22 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,8 +14,8 @@ function Header() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link as={}>Features</Nav.Link>
+            <Nav.Link as={}>Pricing</Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
