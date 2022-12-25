555b9498ea27215a99b593828b110231c5d1be36
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 04:23:08 +0000

    Sun, Dec 25, 2022, 4:23 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,31 +6,31 @@ import '../index.css'
 
 function Header() {
   return (
-    <Navbar fixed="top" expand="sm" bg="dark" className="flex-row Header nav">
-      <Container>
+    // <Navbar fixed="top" expand="sm" bg="dark" className="flex-row Header nav">
+    //   <Container>
 
-        <Navbar.Brand href="/">Tracker</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto App-Link">
-            <Nav.Item>
-              <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
-            </Nav.Item>
+    //     <Navbar.Brand href="/">Tracker</Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav">
+    //       <Nav className="me-auto App-Link">
+    //         <Nav.Item>
+    //           <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
+    //         </Nav.Item>
             
-            <Nav.Item>
-              <Nav.Link as={NavLink} to="/test">Test</Nav.Link>
-            </Nav.Item>
-          </Nav>
-          <Nav className="ms-auto">
-            <Nav.Item>
-              <Nav.Link as={NavLink} to="/logout" end>Logout</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    //         <Nav.Item>
+    //           <Nav.Link as={NavLink} to="/test">Test</Nav.Link>
+    //         </Nav.Item>
+    //       </Nav>
+    //       <Nav className="ms-auto">
+    //         <Nav.Item>
+    //           <Nav.Link as={NavLink} to="/logout" end>Logout</Nav.Link>
+    //         </Nav.Item>
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Container>
 
 
-    </Navbar>
+    // </Navbar>
   );
 }
 
