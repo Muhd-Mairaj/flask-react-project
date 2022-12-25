d5031d791e89a45d11e6d06484fb255f4a257b61
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 00:11:58 +0000

    Sun, Dec 25, 2022, 12:11 AM +00:00

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -7,7 +7,7 @@ function Sidebar() {
   return (
     <Navbar sticky="top" expand="sm" bg="dark" className="flex-row Sidebar nav">
       <Navbar.Brand>
-            <Link to="/" className end>Home</Link>
+            <Link to="/" className="Navbar" end>Home</Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
