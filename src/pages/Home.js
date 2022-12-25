33d15f76f901f8520e0baab1fb64077ee5e9d9af
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 22:54:13 +0000

    Sun, Dec 25, 2022, 10:54 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -39,6 +39,7 @@ function Home() {
           <Col xs={12} sm={7} className="py-1">
             <Form.Group controlId="itemField">
               <Form.Control placeholder="Item name" ref={itemField}/>
+              <Form.text
             </Form.Group>
           </Col>
           <Col xs={12} sm={3} className="py-1">
