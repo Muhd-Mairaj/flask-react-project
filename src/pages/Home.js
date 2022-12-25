151af31b6ea32b3d5744774944eab9907544a432
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 21:07:09 +0000

    Sun, Dec 25, 2022, 9:07 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,7 +19,7 @@ function Home() {
     <Body loggedIn>
       <Container fluid className="Home">
 
-      <Stack direction=""
+      <Stack direction="horizontal"
       {items ? <Table striped bordered variant="dark" responsive="lg">
           <thead>
             <tr>
