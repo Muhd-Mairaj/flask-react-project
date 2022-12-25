36511f2e8cb50ff0903d36fc80b6723ca2608789
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 04:45:20 +0000

    Sun, Dec 25, 2022, 4:45 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,9 +16,8 @@ function Header() {
           <Nav className="me-auto">
             <Nav.Item>
               <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
+            </Nav.Item>
             <Nav.Item>
-            </Nav.Item>
-            </Nav.Item>
               <Nav.Link as={NavLink} to="/test">Test</Nav.Link>
             </Nav.Item>
           </Nav>
