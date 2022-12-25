c726c6329f49c070c213eea0da2376ab09b667ee
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 00:17:14 +0000

    Sun, Dec 25, 2022, 12:17 AM +00:00

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -9,24 +9,24 @@ function Sidebar() {
     <Navbar sticky="top" expand="sm" bg="dark" className="flex-row Sidebar nav">
       <Container>
 
-      <Navbar.Brand href="/">Tracker</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Item>
-            <Nav.Link as={NavLink} to="/" className="App-link" end>Home</Nav.Link>
-          </Nav.Item>
-          
-          <Nav.Item>
-            <Nav.Link as={NavLink} to="/test" className="App-link">Test</Nav.Link>
-          </Nav.Item>
-        </Nav>
-        <Nav className="ms-auto">
-          <Nav.Item>
-            <Nav.Link as={NavLink} to="/logout" className="App-link" end>Logout</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Navbar.Collapse>
+        <Navbar.Brand href="/">Tracker</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/" className="App-link" end>Home</Nav.Link>
+            </Nav.Item>
+            
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/test" className="App-link">Test</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Nav className="ms-auto">
+            <Nav.Item>
+              <Nav.Link as={NavLink} to="/logout" className="App-link" end>Logout</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
 
 
