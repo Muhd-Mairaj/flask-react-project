76767462e383fb047a11cfbd96406d7f093fb0ed
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 07:20:19 +0000

    Sun, Dec 25, 2022, 7:20 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -31,14 +31,14 @@ function Header({ loggedIn }) {
         <Navbar.Collapse id="basic-navbar-nav">
           {loggedIn === true ?
             <>
+              <Nav className="me-auto">
+                  <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
+                  <Nav.Link as={NavLink} to="/test">Test</Nav.Link>
+              </Nav>
+              <Nav>
+                  <Nav.Link as={NavLink} to="/logout" end>Logout</Nav.Link>
+              </Nav>
             </>
-            <Nav className="me-auto">
-                <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
-                <Nav.Link as={NavLink} to="/test">Test</Nav.Link>
-            </Nav>
-            <Nav>
-                <Nav.Link as={NavLink} to="/logout" end>Logout</Nav.Link>
-            </Nav>
           }
         </Navbar.Collapse>
       </Container>
