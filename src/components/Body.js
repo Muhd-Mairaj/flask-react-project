12b089c60d99f30c83a2bcf58fd15e0786f79780
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 21:52:47 +0000

    Sun, Dec 25, 2022, 9:52 PM +00:00

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,7 @@ import Stack from 'react-bootstrap/Stack'
 
 function Body({ loggedIn, children }) {
   return (
-    <Container fluid>
+    // <Container fluid>
       <Stack direction="horizontal" className="Body">
         {loggedIn && <Container fluid className="Content">
             {children}
