61d3dc4ee6d306bb770c537b192ccbbc49d4c20a
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 18:11:20 +0000

    Sun, Dec 25, 2022, 6:11 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -6,7 +6,7 @@ db = SQL("sqlite:///database.db")
 
 @app.route("/profile")
 def profile():
-    query = 
+    query = db.execute
     return jsonify({
         "name": "testing",
         "about": "test"
