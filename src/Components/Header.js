d36c08838ef1f0f812b342fbd7a51b38876b63c5
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 04:33:28 +0000

    Sun, Dec 25, 2022, 4:33 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,42 +7,42 @@ import '../index.css'
 
 function Header() {
   return (
-    <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-
     // <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
     //   <Container>
-
-    //     <Navbar.Brand href="/">Tracker</Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //     <Navbar.Collapse id="basic-navbar-nav">
+    //     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    //     <Navbar.Collapse id="responsive-navbar-nav">
     //       <Nav className="me-auto">
-    //           <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
-    //           <Nav.Link as={NavLink} to="/test">Test</Nav.Link>
+    //         <Nav.Link href="#features">Features</Nav.Link>
+    //         <Nav.Link href="#pricing">Pricing</Nav.Link>
     //       </Nav>
-    //       <Nav className="ms-auto">
-    //           <Nav.Link as={NavLink} to="/logout" end>Logout</Nav.Link>
+    //       <Nav>
+    //         <Nav.Link href="#deets">More deets</Nav.Link>
+    //         <Nav.Link eventKey={2} href="#memes">
+    //           Dank memes
+    //         </Nav.Link>
     //       </Nav>
     //     </Navbar.Collapse>
-
     //   </Container>
     // </Navbar>
+
+    <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
+      <Container>
+
+        <Navbar.Brand href="/">Tracker</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+              <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
+              <Nav.Link as={NavLink} to="/test">Test</Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+              <Nav.Link as={NavLink} to="/logout" end>Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+
+      </Container>
+    </Navbar>
   );
 }
 
