59f8e93b6bbbff24bb30287bfdfcaa7993c46a2a
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 00:27:49 +0000

    Sun, Dec 25, 2022, 12:27 AM +00:00

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -17,7 +17,7 @@ function getData() {
     /** END METHOD 2 */
 
   return (
-    <div className="App">
+    <div className="Home">
       <button onClick={getData}>Get Data</button>
             {profileData && <div>
                   <p>Profile name: {profileData.name}</p>
