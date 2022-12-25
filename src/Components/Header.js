5ba82e1d77ef5329f36582a6038689d917943552
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 04:48:10 +0000

    Sun, Dec 25, 2022, 4:48 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,14 +14,10 @@ function Header() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
               <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
-            <Nav.Item>
               <Nav.Link as={NavLink} to="/test">Test</Nav.Link>
-            </Nav.Item>
           </Nav>
           <Nav className="ms-auto">
-            <Nav.Item>
               <Nav.Link as={NavLink} to="/logout" end>Logout</Nav.Link>
-            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
