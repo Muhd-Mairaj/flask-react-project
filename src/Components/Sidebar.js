8df7d7eee759b28b7b8eea81126805622f854129
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 00:08:18 +0000

    Sun, Dec 25, 2022, 12:08 AM +00:00

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -6,7 +6,7 @@ import '../index.css'
 function Sidebar() {
   return (
     <Navbar sticky="top" expand="sm" bg="dark" className="flex-row Sidebar nav">
-      <Navbar.Brand element={}>Tracker</Navbar.Brand>
+      <Navbar.Brand element={<Navi}>Tracker</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
