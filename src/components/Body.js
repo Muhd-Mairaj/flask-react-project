b20988dc527085b0d5823ccaba028861c6e6e4d2
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 07:46:00 +0000

    Sun, Dec 25, 2022, 7:46 AM +00:00

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,7 +5,7 @@ function Body({ loggedIn, children }) {
   return (
     <Container>
       <Stack direction="horizontal" className="Body">
-        {loggedIn && <Container className="Content">
+        {/* {loggedIn  <Container className="Content"> */}
             {children}
           </Container>
         }
