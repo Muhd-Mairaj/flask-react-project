fb81856cecf603dba115d886f0dae39cc828e612
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 21:45:02 +0000

    Sun, Dec 25, 2022, 9:45 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -38,7 +38,7 @@ function Home() {
           <Col sm={12} lg={3}>
             <Form.Control className="me-auto" placeholder="Item name" type="date"/>
           </Col>
-          <Col sm={12}>
+          <Col sm={12} lg={1}>
             <Button variant="dark" className="" type="submit">Add</Button>
           </Col>
 
