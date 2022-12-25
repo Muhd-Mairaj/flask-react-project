12a51da961fca513faf4172ce14579a53ed02227
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 18:12:20 +0000

    Sun, Dec 25, 2022, 6:12 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -6,7 +6,7 @@ db = SQL("sqlite:///database.db")
 
 @app.route("/profile")
 def profile():
-    query = db.execute("SELECT item, expiry FROM ")
+    query = db.execute("SELECT item, expiry FROM test")
     return jsonify({
         "name": "testing",
         "about": "test"
