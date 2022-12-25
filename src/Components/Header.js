e7b591ee3c74b4b28b349f392e088fa6547b3feb
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 04:26:08 +0000

    Sun, Dec 25, 2022, 4:26 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ import '../index.css'
 
 function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
