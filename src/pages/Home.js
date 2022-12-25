c0b55535779f5316f914571f0052b185e623087d
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 20:16:17 +0000

    Sun, Dec 25, 2022, 8:16 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,7 +29,7 @@ function Home() {
           </thead>
           {Array.from({ length: profileData.items.length }).map((_, index) => (
             <tbody>
-
+              
             </tbody>
               <th key={index}>Column</th>
             ))}
