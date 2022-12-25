2aec8366f7d0c3012001bca1a66530f047d8f5f9
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 07:44:13 +0000

    Sun, Dec 25, 2022, 7:44 AM +00:00

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}></Route>
           <Route path="/test" element={<Test/>}></Route>
-          <Route path="/test" element={<Test/>}></Route>
+          <Route path="/loging" element={<Logi/>}></Route>
         </Routes>
       </BrowserRouter>
       
