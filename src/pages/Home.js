a154d2c24b51324ae8baf681ee5b620060a6940f
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 21:17:13 +0000

    Sun, Dec 25, 2022, 9:17 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,7 +31,7 @@ function Home() {
         <Stack direction="horizontal" gap={3}>
           <Form onSubmit={handleSubmit}>
             <InputField name="itemName" placeholder="Enter name of item..."></InputField>
-            <InputField name="itemName" placeholder="Enter name of item..."></InputField>
+            <InputField name="expiry" placeholder="Enter name of item..."></InputField>
 
           </Form>
         </Stack>
