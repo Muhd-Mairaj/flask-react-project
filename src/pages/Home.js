b833c5f55318dfa188038aaa2769baf8f20e96e2
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 20:17:38 +0000

    Sun, Dec 25, 2022, 8:17 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,7 +31,7 @@ function Home() {
             <tbody>
               <tr>
                 <td>{index}</td>
-                <td key={index}></td>
+                <td key={index}>{}</td>
               </tr>
 
             </tbody>
