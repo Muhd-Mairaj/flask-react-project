c55b67b455280f7b7286c41fac250fd45678a66a
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 07:21:17 +0000

    Sun, Dec 25, 2022, 7:21 AM +00:00

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -42,7 +42,7 @@ function Header({ loggedIn }) {
             :
             <>
               <Nav>
-                  <Nav.Link as={NavLink} to="/login" >Logout</Nav.Link>
+                  <Nav.Link as={NavLink} to="/login">Logout</Nav.Link>
                   <Nav.Link as={NavLink} to="/register">Logout</Nav.Link>
               </Nav>
             </>
