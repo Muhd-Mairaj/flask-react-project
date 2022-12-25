4a022aec834f2ed6a031cfd3e0f3a358691f1c4d
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 21:20:07 +0000

    Sun, Dec 25, 2022, 9:20 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,7 +30,7 @@ function Home() {
 
         <Stack direction="horizontal" gap={3}>
           <Form onSubmit={handleSubmit}>
-            <
+            <Form.Con
 
           </Form>
         </Stack>
