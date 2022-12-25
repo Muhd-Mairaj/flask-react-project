d297d8ece0e1666b3e3b3a0d6d37098a14afe6a8
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-25 23:08:04 +0000

    Sun, Dec 25, 2022, 11:08 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -60,7 +60,7 @@ function Home() {
             </Form.Group>
           </Col>
           <Col xs={12} sm={3} className="py-1">
-            <InputField name="expiry"
+            <InputField name="expiry" type="date"
             <Form.Group controlId="expiry">
               <Form.Control type="date" ref={expiryField}/>
               <Form.Text className="text-danger">{formErrors.expiry}</Form.Text>
