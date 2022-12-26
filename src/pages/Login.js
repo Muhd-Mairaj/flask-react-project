28c96fe043e1dcca157ef91370d6509e20484e3c
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 19:40:23 +0000

    Mon, Dec 26, 2022, 7:40 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -59,23 +59,6 @@ function Login() {
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {/* <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-          </Form> */}
           <Form onSubmit={handleSubmit}>
             <InputField
               name="username" label="Username" placeholder="Username"
