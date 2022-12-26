ec5b8d0e3229a4a85f3bc8cf4ba27b8a867aa00c
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 19:47:02 +0000

    Mon, Dec 26, 2022, 7:47 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -43,7 +43,7 @@ function Login() {
   
   return (
     <Body loggedIn>
-      <h1>Login</h1>
+      <h2>Login</h2>
       {/* <Form onSubmit={handleSubmit}>
           <InputField
             name="username" label="Username" placeholder="Username"
