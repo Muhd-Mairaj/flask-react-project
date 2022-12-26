502b1cae54d1376b4bab104e196488d41ca875a2
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 14:31:53 +0000

    Mon, Dec 26, 2022, 2:31 PM +00:00

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Test from './pages/Test';
 import Login from './pages/Login';
-import Lo
 // import logo from './logo.svg';
 // import './App.css';
 
