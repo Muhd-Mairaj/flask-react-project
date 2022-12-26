b88cf4d096fb18501721d11003152e29cf1135fc
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 20:17:07 +0000

    Mon, Dec 26, 2022, 8:17 PM +00:00

---
+++ src/pages/Registration.js
@@ -0,0 +1,88 @@
+import { useState, useEffect, useRef } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Body from '../components/Body';
+import InputField from '../components/InputField';
+
+
+function Login() {
+  const [formErrors, setFormErrors] = useState({})
+  const usernameField = useRef()
+  const passwordField = useRef()
+
+  useEffect(() => {
+    usernameField.current.focus();
+  }, []);
+
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    const username = usernameField.current.value
+    const password = passwordField.current.value
+
+    const errors = {};
+    if (!username) {
+      errors.username = "Username field must be filled"
+    }
+    if (!password) {
+      errors.password = "Password field must be filled"
+    }
+
+    setFormErrors(errors)
+    if (Object.keys(errors).length > 0) {
+      return
+    }
+
+    // Login
+    alert("Logging in")
+  }
+  
+  return (
+    <Body loggedIn>
+      {/* <h2>Login</h2> */}
+      {/* <Form onSubmit={handleSubmit}>
+          <InputField
+            name="username" label="Username" placeholder="Username"
+            error={formErrors.username} fieldRef={usernameField}
+            class_name="mx-auto w-auto"
+          />
+          <InputField
+            name="password" label="Password" type="password" placeholder="Password"
+            error={formErrors.password} fieldRef={passwordField} 
+            class_name="mx-auto w-auto"
+          />
+          <Button variant="primary" type="submit" className="mx-auto">Login</Button>
+      </Form>
+    </Body> */}
+      <Form onSubmit={handleSubmit}>
+        <Col>
+          <Row className="justify-content-center">
+            <Col xs={12} sm={6} md={5} lg={4} xl={3}>
+              <InputField
+                name="username" label="Username" placeholder="Username"
+                error={formErrors.username} fieldRef={usernameField} />
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            <Col xs={12} sm={6} md={5} lg={4} xl={3}>
+              <InputField
+              name="password" label="Password" type="password" placeholder="Password"
+              error={formErrors.password} fieldRef={passwordField} />
+            </Col>
+          </Row>
+          <Row className="justify-content-center py-3">
+            <Col xs={10} sm={3} xl={1}>
+              <Button variant="dark" className="w-100" type="submit">Login</Button>
+            </Col>
+          </Row>
+        </Col>
+      </Form>
+    </Body>
+  );
+}
+
+export default Login
