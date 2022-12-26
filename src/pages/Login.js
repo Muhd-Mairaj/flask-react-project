94561dbda1de45b44f2ee7e5f830609ab3a5938a
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 22:54:01 +0000

    Mon, Dec 26, 2022, 10:54 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-r'
+import { Link } from 'react-router-dom'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
