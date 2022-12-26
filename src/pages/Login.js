c77752d42c6741b4634e67c87f8afc42488b2857
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 14:54:27 +0000

    Mon, Dec 26, 2022, 2:54 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -44,7 +44,7 @@ function Login() {
       <h1>Login</h1>
       <Form onSubmit={handleSubmit}>
         <InputField
-          name="username" label="Username /or email address"
+          name="username" label="Username"
           error={formErrors.username} fieldRef={usernameField} />
         <InputField
           name="password" label="Password" type="password"
