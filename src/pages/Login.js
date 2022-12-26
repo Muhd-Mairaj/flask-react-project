3afb8643516350d6206753aced66ab634dae901b
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 14:58:53 +0000

    Mon, Dec 26, 2022, 2:58 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -51,7 +51,7 @@ function Login() {
               name="username" label="Username" placeholder="Username"
               error={formErrors.username} fieldRef={usernameField} />
           </Row>
-          <Row sxs={12}>
+          <Row xs={12}>
             <InputField
             name="password" label="Password" type="password" placeholder="Password"
             error={formErrors.password} fieldRef={passwordField} />
