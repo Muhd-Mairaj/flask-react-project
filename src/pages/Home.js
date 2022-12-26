c7bdb93506ab56d02e443d9b19a35ab5f5305916
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 14:20:56 +0000

    Mon, Dec 26, 2022, 2:20 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -77,7 +77,7 @@ function Home() {
 
       <hr></hr>
 
-      {items ? <Table striped bordered variant="dark" responsive="lg" className="Home">
+      {items ? <Table striped bordered variant="dark" responsive="lg" className="">
           <thead>
             <tr>
               <th>#</th>
