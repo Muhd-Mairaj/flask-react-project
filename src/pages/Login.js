c3495e135eafabd6ba0a9f79f7052d52a73308a1
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 18:57:03 +0000

    Mon, Dec 26, 2022, 6:57 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -58,7 +58,7 @@ function Login() {
     //       <Button variant="primary" type="submit" className="mx-auto">Login</Button>
     //   </Form>
       <Form onSubmit={handleSubmit}>
-            <Row xs={12} sm={6} class_name>
+            <Row xs={12} sm={6} cla>
               <Col className="">
               <InputField
                 name="username" label="Username" placeholder="Username"
