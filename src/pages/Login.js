45d11f72cad0a5c5b71654e7b6b3c3129559d5d1
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 14:36:48 +0000

    Mon, Dec 26, 2022, 2:36 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,7 +4,7 @@ import Body from '../components/Body';
 function Login() {
   const [formErrors, setFormErrors] = useState({})
   const usernameField = useRef()
-  const usernameField = useRef()
+  const passwordField = useRef()
 
   
   return (
