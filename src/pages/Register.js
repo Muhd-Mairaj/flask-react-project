a54df4ae050b190dbb227d11829e77200a216a38
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 20:22:06 +0000

    Mon, Dec 26, 2022, 8:22 PM +00:00

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -49,20 +49,6 @@ function Register() {
   return (
     <Body loggedIn>
       {/* <h2>Register</h2> */}
-      {/* <Form onSubmit={handleSubmit}>
-          <InputField
-            name="username" label="Username" placeholder="Username"
-            error={formErrors.username} fieldRef={usernameField}
-            class_name="mx-auto w-auto"
-          />
-          <InputField
-            name="password" label="Password" type="password" placeholder="Password"
-            error={formErrors.password} fieldRef={passwordField} 
-            class_name="mx-auto w-auto"
-          />
-          <Button variant="primary" type="submit" className="mx-auto">Login</Button>
-      </Form>
-    </Body> */}
       <Form onSubmit={handleSubmit}>
         <Col>
           <Row className="justify-content-center">
