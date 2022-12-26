e31b94a2b3fd958c685950be103718e1486b97ca
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 19:31:23 +0000

    Mon, Dec 26, 2022, 7:31 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -53,7 +53,7 @@ function Login() {
       <Modal 
         show={show} 
         onHide={handleClose}
-        size="sm">
+        size="md">
         <Modal.Header>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
