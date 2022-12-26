a40fc7d0408028472e87ae638e758e2ba16936c7
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 14:57:46 +0000

    Mon, Dec 26, 2022, 2:57 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react=bootstrap/Row';
-import COl from 'react=bootstrap/Row';
+import Col from 'react=bootstrap/Col';
 
 import Body from '../components/Body';
 import InputField from '../components/InputField';
