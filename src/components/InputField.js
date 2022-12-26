6d0bf56e8bf5c09af977c269923248ac74095c59
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 18:33:08 +0000

    Mon, Dec 26, 2022, 6:33 PM +00:00

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -10,7 +10,7 @@ function InputField(
         type={type || 'text'}
         placeholder={placeholder}
         ref={fieldRef}
-        class
+        className={class_name}
       />
       <div className="mx-1">
         <Form.Text className="text-danger text-center text-wrap">{error}</Form.Text>
