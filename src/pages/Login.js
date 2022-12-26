83663b1597cedd990e73be18a1a7826df43ed68c
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 18:36:12 +0000

    Mon, Dec 26, 2022, 6:36 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -55,7 +55,7 @@ function Login() {
             error={formErrors.password} fieldRef={passwordField} 
             class_name="mx-auto w-auto"
           />
-          <Button variant="primary" type="submit" className="mx-auto w-auto">Login</Button>
+          <Button variant="primary" type="submit" className="mx-auto">Login</Button>
       </Form>
     </Body>
     //   <Form onSubmit={handleSubmit}>
