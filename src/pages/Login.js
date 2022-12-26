bc60086eb90662d7ff5d26130123e05cd55d42d9
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 14:34:58 +0000

    Mon, Dec 26, 2022, 2:34 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,20 +2,20 @@ import Body from '../components/Body';
 
 
 function Login() {
-    return (
-      <Body>
-        <h1>Login</h1>
-        <Form onSubmit={onSubmit}>
-          <InputField
-            name="username" label="Username or email address"
-            error={formErrors.username} fieldRef={usernameField} />
-          <InputField
-            name="password" label="Password" type="password"
-            error={formErrors.password} fieldRef={passwordField} />
-          <Button variant="primary" type="submit">Login</Button>
-        </Form>
-      </Body>
-    );
+  return (
+    <Body>
+      <h1>Login</h1>
+      <Form onSubmit={onSubmit}>
+        <InputField
+          name="username" label="Username or email address"
+          error={formErrors.username} fieldRef={usernameField} />
+        <InputField
+          name="password" label="Password" type="password"
+          error={formErrors.password} fieldRef={passwordField} />
+        <Button variant="primary" type="submit">Login</Button>
+      </Form>
+    </Body>
+  );
 }
 
 export default Login
