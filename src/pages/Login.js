0c5f4e57d6d72e0a7c5d9e25ad1d7b0a1f79e6a4
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 19:25:13 +0000

    Mon, Dec 26, 2022, 7:25 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -67,17 +67,17 @@ function Login() {
             </Form.Group>
           </Form>
           <Form onSubmit={handleSubmit}>
-          <InputField
-            name="username" label="Username" placeholder="Username"
-            error={formErrors.username} fieldRef={usernameField}
-            class_name="mx-auto w-auto"
-          />
-          <InputField
-            name="password" label="Password" type="password" placeholder="Password"
-            error={formErrors.password} fieldRef={passwordField} 
-            class_name="mx-auto w-auto"
-          />
-          <Button variant="primary" type="submit" className="mx-auto">Login</Button>
+            <InputField
+              name="username" label="Username" placeholder="Username"
+              error={formErrors.username} fieldRef={usernameField}
+              class_name="mx-auto w-auto"
+            />
+            <InputField
+              name="password" label="Password" type="password" placeholder="Password"
+              error={formErrors.password} fieldRef={passwordField} 
+              class_name="mx-auto w-auto"
+            />
+            <Button variant="primary" type="submit" className="mx-auto">Login</Button>
           </Form>
         </Modal.Body>
         <Modal.Footer>
