84089b15b1e89bbae35be4a901cc86a7b64b7870
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 18:06:04 +0000

    Mon, Dec 26, 2022, 6:06 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -51,7 +51,7 @@ function Login() {
             error={formErrors.username} fieldRef={usernameField}
           />
         </div>
-        <div
+        <div className=
         <InputField
           name="password" label="Password" type="password" placeholder="Password"
           error={formErrors.password} fieldRef={passwordField} 
