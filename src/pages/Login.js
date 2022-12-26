ec005d44171c05bc203d7252206cba9ff6774f77
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 14:57:12 +0000

    Mon, Dec 26, 2022, 2:57 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -44,7 +44,7 @@ function Login() {
     <Body loggedIn>
       <h1>Login</h1>
       <Form onSubmit={handleSubmit}>
-        <Col className=
+        <Col className="justify-content-center"
         <Row>
           <InputField
             name="username" label="Username" placeholder="Username"
