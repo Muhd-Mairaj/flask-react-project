84a18ea02717bf1487750f1ab5cc99e7517b1d70
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 20:22:11 +0000

    Mon, Dec 26, 2022, 8:22 PM +00:00

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -48,7 +48,7 @@ function Register() {
   
   return (
     <Body loggedIn>
-      <h2>Register</h2>
+      {/* <h2>Register</h2> */}
       <Form onSubmit={handleSubmit}>
         <Col>
           <Row className="justify-content-center">
