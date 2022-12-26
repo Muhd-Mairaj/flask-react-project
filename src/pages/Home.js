5b43c7b7649b27c436bc2e84bdd027e166e320c4
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-26 20:00:26 +0000

    Mon, Dec 26, 2022, 8:00 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -51,7 +51,7 @@ function Home() {
   return (
     <Body loggedIn>
       <Form onSubmit={handleSubmit}>
-        <Row className="HorizontalForm justify-co">
+        <Row className="HorizontalForm justify-contents">
           <Col xs={12} sm={7} className="">
             <InputField
               name="item"
