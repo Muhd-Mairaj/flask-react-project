89989d81413c3433bcf1df84fee97a4fd07dd757
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-27 01:24:17 +0000

    Tue, Dec 27, 2022, 1:24 AM +00:00

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import { useNavigate } 
+import { useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
