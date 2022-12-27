b62a3488469ec305703667dafa9988dfbc8a8352
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-27 01:03:48 +0000

    Tue, Dec 27, 2022, 1:03 AM +00:00

--- src/contexts/FlashProvider.js
+++ src/contexts/FlashProvider.js
@@ -5,7 +5,7 @@ export const FlashContext = useContext()
 
 
 export default function FlashProvider({ children }) {
-  const [ flashTimer, setFlashTimer ] = useState()
+  const [ flashTimer, setFlashTimer ] = useState(0)
 
   function flash(message, type, duration = 10) {
     if ()
