0032924193f65421866015def9c1cf44e84eb320
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-27 01:03:13 +0000

    Tue, Dec 27, 2022, 1:03 AM +00:00

--- src/contexts/FlashProvider.js
+++ src/contexts/FlashProvider.js
@@ -4,10 +4,10 @@ import { useContext } from 'react';
 export const FlashContext = useContext()
 
 
-export default FlashProvider({ children }) {
+export default function FlashProvider({ children }) {
 
   flash(message, type, duration = 10) {
-    
+
   }
   
 }
