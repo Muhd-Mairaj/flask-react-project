85cee9c3229e6123ae2c329b45fc93dd9de4f1ac
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-27 01:29:37 +0000

    Tue, Dec 27, 2022, 1:29 AM +00:00

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,12 @@
 import Container from 'react-bootstrap/Container'
 import FlashMessage from './FlashMessage';
+import FlashProvider from './contexts/FlashProvider'
 
 
 function Body({ loggedIn, children }) {
   return (
     <>
-    
+
         {loggedIn && <Container fluid className="Content">
             <FlashMessage />
             {children}
