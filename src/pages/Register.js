0bb47ad962a4a723ec93cb768973d0a727424e9b
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-27 01:21:03 +0000

    Tue, Dec 27, 2022, 1:21 AM +00:00

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -7,7 +7,7 @@ import Col from 'react-bootstrap/Col';
 import Body from '../components/Body';
 import InputField from '../components/InputField';
 
-import useFlash from 
+import useFlash from ../con
 
 
 function Register() {
