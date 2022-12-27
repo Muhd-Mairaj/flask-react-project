b95f92f6588b69ffd93cdc526f2e775d88a3d92c
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-27 01:29:42 +0000

    Tue, Dec 27, 2022, 1:29 AM +00:00

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,7 +6,7 @@ import FlashProvider from './contexts/FlashProvider'
 function Body({ loggedIn, children }) {
   return (
     <>
-
+      
         {loggedIn && <Container fluid className="Content">
             <FlashMessage />
             {children}
