caba36c84c5aa8d7057e6a64b5d36ee318479f97
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 16:15:10 +0000

    Wed, Dec 28, 2022, 4:15 PM +00:00

--- src/App.js
+++ src/App.js
@@ -12,6 +12,21 @@ import FlashProvider from './contexts/FlashProvider'
 // import logo from './logo.svg';
 // import './App.css';
 
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-pt44rf70pk8szrwn.us.auth0.com"
+    clientId="uja7uL1qUdRfNYnyGGrQdFfOQeEAhECv"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
 
 function App() {
   return (
