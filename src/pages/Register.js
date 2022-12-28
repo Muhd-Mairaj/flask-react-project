f324967b5ac64deddb08714442edcd51db27cfbd
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 23:55:08 +0000

    Wed, Dec 28, 2022, 11:55 PM +00:00

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -64,7 +64,7 @@ function Register() {
       })
     })
 
-    console.log(response.body.errors.json)
+    console.log(response.body.errors)
     if (!response.ok) {
       // setFormErrors()
     }
