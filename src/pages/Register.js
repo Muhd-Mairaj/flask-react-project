71f6d82c8a30a2e78381a3372e6cba7b1afbdb9a
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 23:10:05 +0000

    Wed, Dec 28, 2022, 11:10 PM +00:00

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -53,7 +53,7 @@ function Register() {
     // Resiter
     flash('You have successfully registered!', 'success');
 
-    const response = await fetch("/register", methods)
+    const response = await fetch("/register)
     // navigate('/login');
 
   }
