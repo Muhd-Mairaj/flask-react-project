9547f2fb40f56fa6134eee32e1f64a66d6854151
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 17:57:33 +0000

    Wed, Dec 28, 2022, 5:57 PM +00:00

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -14,25 +14,6 @@
 
 // export default Test;
 
-// import React from "react";
-// import { useAuth0 } from "@auth0/auth0-react";
-
-// const LogoutButton = () => {
-//   const { logout } = useAuth0();
-//   const { loginWithRedirect } = useAuth0();
-
-//   return (
-//     <>
-//       <button onClick={() => logout({ returnTo: "http://127.0.0.1:3000/login" })}>
-//         Log Out
-//       </button>
-//       <button onClick={() => loginWithRedirect()}>Log In</button>
-//     </>
-//   );
-// };
-
-// export default LogoutButton;
-
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
