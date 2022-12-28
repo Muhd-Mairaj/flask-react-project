faf489a184715444ab4e1506307e7cf193b3a414
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 23:41:02 +0000

    Wed, Dec 28, 2022, 11:41 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -23,7 +23,7 @@ def register():
   username = request.json.get("username")
   password = request.json.get("password")
 
-  db_query = db.execute("SELECT username FROM users")
+  db_query = db.execute("SELECT username FROM users WHERE ")
   print(db_query)
   # print(username, password)
   return {"yes": "nice"}
