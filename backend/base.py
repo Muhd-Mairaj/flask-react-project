53b6731798899cc7cb331b146bc421bc741d70a8
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 23:38:24 +0000

    Wed, Dec 28, 2022, 11:38 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -20,7 +20,7 @@ def profile():
 @app.route("/register", methods=["POST"])
 def register():
 
-  req = request.j()
+  req = request.json()
   print(req)
   # password = request.json.get("password")
   # print(f"{username = }")
