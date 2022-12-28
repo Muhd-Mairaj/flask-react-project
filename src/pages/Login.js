1f53e7f6ea8d02be117dc2b4d6c4775df7e0332d
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 22:17:30 +0000

    Wed, Dec 28, 2022, 10:17 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -89,9 +89,7 @@ function Login() {
           <p className="py-2">Don&apos;t have an account? <Link to="/register">Register here</Link>!</p>
         </Col>
       </Row>
-
     </Body>
-  );
 }
 
 export default Login
