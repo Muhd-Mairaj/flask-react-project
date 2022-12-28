58e919d35942b933060b0db67bceeeaf1ecd2749
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 16:29:04 +0000

    Wed, Dec 28, 2022, 4:29 PM +00:00

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,9 @@ import Container from "react-bootstrap/Container";
 // import NavDropdown from 'react-bootstrap/NavDropdown'
 import { NavLink } from 'react-router-dom';
 import '../index.css'
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Header({ loggedIn }) {
-  import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
