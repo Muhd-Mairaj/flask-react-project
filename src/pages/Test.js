82cafe8a1f112316568c37438756a9f03bf1e161
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 16:22:28 +0000

    Wed, Dec 28, 2022, 4:22 PM +00:00

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import Body from '../components/Body';
+// import React from 'react';
+// import Body from '../components/Body';
 
 
-function Test() {
-  return (
-    <Body loggedIn>
-      <div className="Test">
-        <h2>hello there</h2>
-      </div>
-    </Body>
-  )
-}
+// function Test() {
+//   return (
+//     <Body loggedIn>
+//       <div className="Test">
+//         <h2>hello there</h2>
+//       </div>
+//     </Body>
+//   )
+// }
 
-export default Test;
+// export default Test;
+
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+export default LoginButton;
