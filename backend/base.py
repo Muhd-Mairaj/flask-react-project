f543460b1451713250c56f9f413c189147cc0267
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 23:18:35 +0000

    Wed, Dec 28, 2022, 11:18 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, requet, session
+from flask import Flask, jsonify, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from cs50 import SQL
