8ba7734ce611fde30b1c604a3f2c1f4b1881a75e
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 22:21:12 +0000

    Wed, Dec 28, 2022, 10:21 PM +00:00

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,9 +7,6 @@ import {useAuth0} from '@auth0/auth0-react';
 function Body({ loggedIn, children }) {
   const { isLoading, isAuthenticated } = useAuth0()
 
-  if (isLoading) {
-    return
-  }
   return (
     <>
         {loggedIn && <Container fluid className="Content">
