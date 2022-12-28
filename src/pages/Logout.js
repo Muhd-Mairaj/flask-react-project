715f76ffbf909ceccf47da072f568bbc4dd8941e
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 22:17:54 +0000

    Wed, Dec 28, 2022, 10:17 PM +00:00

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,14 +1,1 @@
 import React from 'react';
-import {useAuth0} from '@auth0/auth0-react';
-
-
-function Logout() {
-  const { logout } = useAuth0()
-
-  return (
-    logout({ returnTo: "http://127.0.0.1:3000/login" })
-  )
-}
-
-
-export default Logout;
