ebe0add44ba915350a993abeef286ee7e237959f
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 23:38:56 +0000

    Wed, Dec 28, 2022, 11:38 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -23,7 +23,7 @@ def register():
   username = request.json.get("username")
   password = request.json.get("password")
 
-  
+  db_query = 
   # print(username, password)
   return {"yes": "nice"}
 
