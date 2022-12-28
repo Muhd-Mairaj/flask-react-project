97911a5b10f42003f54cd8cc697883e6c195bddc
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 16:41:50 +0000

    Wed, Dec 28, 2022, 4:41 PM +00:00

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -14,21 +14,44 @@
 
 // export default Test;
 
+// import React from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
+
+// const LogoutButton = () => {
+//   const { logout } = useAuth0();
+//   const { loginWithRedirect } = useAuth0();
+
+//   return (
+//     <>
+//       <button onClick={() => logout({ returnTo: "http://127.0.0.1:3000/login" })}>
+//         Log Out
+//       </button>
+//       <button onClick={() => loginWithRedirect()}>Log In</button>
+//     </>
+//   );
+// };
+
+// export default LogoutButton;
+
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const LogoutButton = () => {
-  const { logout } = useAuth0();
-  const { loginWithRedirect } = useAuth0();
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
 
   return (
-    <>
-      <button onClick={() => logout({ returnTo: "http://127.0.0.1:3000/login" })}>
-        Log Out
-      </button>
-      <button onClick={() => loginWithRedirect()}>Log In</button>
-    </>
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
   );
 };
 
-export default LogoutButton;
+export default Profile;
