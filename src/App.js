6c501e7df2061164955b7b8ec7d3d46be600a46d
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 16:15:55 +0000

    Wed, Dec 28, 2022, 4:15 PM +00:00

--- src/App.js
+++ src/App.js
@@ -13,14 +13,7 @@ import FlashProvider from './contexts/FlashProvider'
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="dev-pt44rf70pk8szrwn.us.auth0.com"
-    clientId="uja7uL1qUdRfNYnyGGrQdFfOQeEAhECv"
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
-  document.getElementById("root")
+  
 );
 
 function App() {
