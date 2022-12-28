1620ce794b7aa74982f0f72ada298ec5f31b9c94
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 22:21:17 +0000

    Wed, Dec 28, 2022, 10:21 PM +00:00

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,8 @@
 import Container from 'react-bootstrap/Container'
 import FlashMessage from './FlashMessage';
-import {useAuth0} from '@auth0/auth0-react';
-
 
 
 function Body({ loggedIn, children }) {
-  const { isLoading, isAuthenticated } = useAuth0()
-
   return (
     <>
         {loggedIn && <Container fluid className="Content">
