09b49983c9f16cefbd7070ffceefa34e93702270
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 22:15:09 +0000

    Wed, Dec 28, 2022, 10:15 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -43,54 +43,54 @@ function Login() {
     alert("Logging in")
   }
   
-    // <Body loggedIn>
-    //   {/* <h2>Login</h2> */}
-    //   {/* <Form onSubmit={handleSubmit}>
-    //       <InputField
-    //         name="username" label="Username" placeholder="Username"
-    //         error={formErrors.username} fieldRef={usernameField}
-    //         class_name="mx-auto w-auto"
-    //       />
-    //       <InputField
-    //         name="password" label="Password" type="password" placeholder="Password"
-    //         error={formErrors.password} fieldRef={passwordField} 
-    //         class_name="mx-auto w-auto"
-    //       />
-    //       <Button variant="primary" type="submit" className="mx-auto">Login</Button>
-    //   </Form>
-    // </Body> */}
-    //   <Form onSubmit={handleSubmit}>
-    //     <Col>
-    //       <Row className="justify-content-center">
-    //         <Col xs={12} sm={6} md={5} lg={4} xl={3}>
-    //           <InputField
-    //             name="username" label="Username" placeholder="Username"
-    //             error={formErrors.username} fieldRef={usernameField} />
-    //         </Col>
-    //       </Row>
-    //       <Row className="justify-content-center">
-    //         <Col xs={12} sm={6} md={5} lg={4} xl={3}>
-    //           <InputField
-    //           name="password" label="Password" type="password" placeholder="Password"
-    //           error={formErrors.password} fieldRef={passwordField} />
-    //         </Col>
-    //       </Row>
-    //       <Row className="justify-content-center py-3">
-    //         <Col xs={10} sm={3} lg={2} xl={1}>
-    //           <Button variant="dark" className="w-100" onClick={() => loginWithRedirect()} type="submit">Login</Button>
-    //         </Col>
-    //       </Row>
-    //     </Col>
-    //   </Form>
+    <Body loggedIn>
+      {/* <h2>Login</h2> */}
+      {/* <Form onSubmit={handleSubmit}>
+          <InputField
+            name="username" label="Username" placeholder="Username"
+            error={formErrors.username} fieldRef={usernameField}
+            class_name="mx-auto w-auto"
+          />
+          <InputField
+            name="password" label="Password" type="password" placeholder="Password"
+            error={formErrors.password} fieldRef={passwordField} 
+            class_name="mx-auto w-auto"
+          />
+          <Button variant="primary" type="submit" className="mx-auto">Login</Button>
+      </Form>
+    </Body> */}
+      <Form onSubmit={handleSubmit}>
+        <Col>
+          <Row className="justify-content-center">
+            <Col xs={12} sm={6} md={5} lg={4} xl={3}>
+              <InputField
+                name="username" label="Username" placeholder="Username"
+                error={formErrors.username} fieldRef={usernameField} />
+            </Col>
+          </Row>
+          <Row className="justify-content-center">
+            <Col xs={12} sm={6} md={5} lg={4} xl={3}>
+              <InputField
+              name="password" label="Password" type="password" placeholder="Password"
+              error={formErrors.password} fieldRef={passwordField} />
+            </Col>
+          </Row>
+          <Row className="justify-content-center py-3">
+            <Col xs={10} sm={3} lg={2} xl={1}>
+              <Button variant="dark" className="w-100" onClick={() => loginWithRedirect()} type="submit">Login</Button>
+            </Col>
+          </Row>
+        </Col>
+      </Form>
       
-    //   <Row className="justify-content-center">
-    //     <Col xs={12} sm={6} md={5} lg={4} xl={3}>
-    //       <hr />
-    //       <p className="py-2">Don&apos;t have an account? <Link to="/register">Register here</Link>!</p>
-    //     </Col>
-    //   </Row>
+      <Row className="justify-content-center">
+        <Col xs={12} sm={6} md={5} lg={4} xl={3}>
+          <hr />
+          <p className="py-2">Don&apos;t have an account? <Link to="/register">Register here</Link>!</p>
+        </Col>
+      </Row>
 
-    // </Body>
+    </Body>
   );
 }
 
