0b5dea3edb7e99c6d8e841613e7eba6ab026d625
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-28 23:06:35 +0000

    Wed, Dec 28, 2022, 11:06 PM +00:00

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -53,7 +53,7 @@ function Register() {
     // Resiter
     flash('You have successfully registered!', 'success');
 
-    response = async () {}
+    response = async () {}()
     // navigate('/login');
 
   }
