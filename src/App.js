3924829bb26e7d1415eea5c1f40cbee84e3827fc
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 23:16:06 +0000

    Thu, Dec 29, 2022, 11:16 PM +00:00

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
                   </Routes>
                   {/* </PrivateComponent> */}
 
-              </UserProvider>
+              {/* </UserProvider> */}
             </ApiProvider>
           </FlashProvider>
 
