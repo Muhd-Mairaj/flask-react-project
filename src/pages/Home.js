4eef4991f4c39c0138643d2865df5245b942f8f3
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 08:25:26 +0000

    Thu, Dec 29, 2022, 8:25 AM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,7 @@ function Home() {
 
   useEffect(() => {
     if (localStorage.getItem('accessToken') === null) {
-      
+      navigate()
     }
     fetch("/profile").then(response => response.json()).then(data => {
       console.log(data.items)
