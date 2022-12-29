3ddd75c882b7fdf15759d51173492a56ed4cf895
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 07:46:14 +0000

    Thu, Dec 29, 2022, 7:46 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -10,7 +10,6 @@ from cs50 import SQL
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf"
-session["SECRET_KEY"] = "192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf"
 app.config["SESSION_PERMANENT"] = False
 Session(app)
 
