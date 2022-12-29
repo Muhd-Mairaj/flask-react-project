9f1c9300f5dd5b033a2836c35c8d0f5ce3950cda
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 04:58:16 +0000

    Thu, Dec 29, 2022, 4:58 AM +00:00

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -52,7 +52,7 @@ function Register() {
     const response = await fetch("/register", {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        Content-Type': 'application/json',
       },
       body: JSON.stringify({
         username: usernameField.current.value,
