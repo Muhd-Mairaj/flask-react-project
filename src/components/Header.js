b0d2f7e12ad33d90ae99627ed67457ec0250acb3
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 08:50:12 +0000

    Thu, Dec 29, 2022, 8:50 AM +00:00

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function Header({ loggedIn }) {
   const [ isAuthenticated, setIsAuthenticated ] = useState(true)
 
   useEffect(() => {
-    if 
+    if localStorage.getItem
   }, [])
   
   return (
