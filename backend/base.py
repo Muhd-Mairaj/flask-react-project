ff589907a195039672b3ae3ae81f3bd7fbd4097c
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 08:21:37 +0000

    Thu, Dec 29, 2022, 8:21 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -40,7 +40,7 @@ def unauthorized():
 
 
 @app.route("/profile")
-@app.login_required
+@basic_auth.login_required
 def profile():
     user_id = 1
     items = db.execute("SELECT item, expiry FROM test WHERE id=?", user_id)
