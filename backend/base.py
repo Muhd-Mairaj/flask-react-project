494d8373a577308c34d3b59e2d640cedff858035
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 05:35:32 +0000

    Thu, Dec 29, 2022, 5:35 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -17,8 +17,8 @@ db = SQL("sqlite:///database.db")
 def verify_password(username, password):
   rows = db.execute("SELECT id, username, hash FROM users WHERE username = ?", username)
 
-  if len(db_query) == 1 and check_password_hash(rows[0][], password):
-    session["user_id"] = rows[0][]
+  if len(db_query) == 1 and check_password_hash(rows[0]["hash"], password):
+    session["user_id"] = rows[0]["id"]
     return username
 
 
