2818f6df362d26a7da2d41fa67377bd740355934
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 23:18:47 +0000

    Thu, Dec 29, 2022, 11:18 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -14,7 +14,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_COOKIE_NAME"] = "session"
 app.config["SESSION_TYPE"] = "filesystem"
 
-
 Session(app)
 
 db = SQL("sqlite:///database.db")
