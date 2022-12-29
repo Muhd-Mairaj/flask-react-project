5de15cef0cade36adf943b7c41afa323ea7a7fb3
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 23:19:07 +0000

    Thu, Dec 29, 2022, 11:19 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -7,7 +7,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from cs50 import SQL
 
 
-# initialise app
+# app
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf"
