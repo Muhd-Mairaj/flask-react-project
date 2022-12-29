c9bacbe4cf2b2ba24e46d8e6106006d546e376b6
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 22:05:51 +0000

    Thu, Dec 29, 2022, 10:05 PM +00:00

--- src/contexts/UserProvider.js
+++ src/contexts/UserProvider.js
@@ -6,7 +6,6 @@ const UserContext = createContext()
 export default function UserProvider({ children }) {
   const [user, setUser] = useState()
   const { api } = useApi()
-  console.log(user)
 
   useEffect(() => {
     (async () => {
