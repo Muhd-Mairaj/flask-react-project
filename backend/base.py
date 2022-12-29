30176675e27b966433602a1a51bb151b03de419c
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 23:17:41 +0000

    Thu, Dec 29, 2022, 11:17 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, make_response, request, session
 from flask_session import Session
-from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from secrets import token_urlsafe
 # from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
