4b60871c96308637428bae2356427d6bc92ad655
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 00:11:38 +0000

    Thu, Dec 29, 2022, 12:11 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -55,7 +55,7 @@ def register():
 
   
   # generate password hash
-  password_hash = generate_password_hash
+  password_hash = generate_password_hash(password)
   # add user to db
   db.execute("INSERT INTO users (username, passoword) VALUES(?, ?)")
 
