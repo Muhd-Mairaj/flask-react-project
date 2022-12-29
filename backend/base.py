75c0ac22fe2eb4410ddcb09806724b28707d6a19
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 07:53:16 +0000

    Thu, Dec 29, 2022, 7:53 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf"
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = ""
+app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
 
