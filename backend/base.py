78092019c6e743cd83c4218b44bbd226514acb40
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 23:19:39 +0000

    Thu, Dec 29, 2022, 11:19 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -11,6 +11,7 @@ from cs50 import SQL
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf"
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_COOKIE_NAME"] = "session"
 app.config["SESSION_TYPE"] = "filesystem"
