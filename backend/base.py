747f2ee4eaa21e41681bf9434368c643a3fbdc5f
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 23:17:10 +0000

    Thu, Dec 29, 2022, 11:17 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -26,30 +26,6 @@ db = SQL("sqlite:///database.db")
 # app.config["SESSION_PERMANENT"] = False
 # SESSION_COOKIE_SECURE
 
-@basic_auth.verify_password
-def verify_password(username, password):
-  rows = db.execute("SELECT id, username, hash FROM users WHERE username = ?", username)
-
-  if len(rows) == 1 and check_password_hash(rows[0]["hash"], password):
-    session["user_id"] = rows[0]["id"]
-    return session["user_id"]
-
-
-@basic_auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
-
-
-@token_auth.verify_token
-def verify_token(token):
-  if token == session.get("access_token"):
-    return session["user_id"]
-
-
-@token_auth.error_handler
-def unauthorized():
-    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
-
 
 @app.route("/profile", methods=["get"])
 @token_auth.login_required
