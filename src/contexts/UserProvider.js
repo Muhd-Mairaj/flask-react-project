7fb5af2b1bfc479785c8525105f580e048c65b7f
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 20:42:14 +0000

    Thu, Dec 29, 2022, 8:42 PM +00:00

--- src/contexts/UserProvider.js
+++ src/contexts/UserProvider.js
@@ -4,5 +4,5 @@ import React, { useEFfect, useState, useContext, createContext } from 'react';
 const UserContext = createContext()
 
 export default function UserProvider() {
-  
+  const [user, setUser] = useState
 }
