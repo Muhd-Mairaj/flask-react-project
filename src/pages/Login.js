f1310a5b3b4202db428f1d44eea6452aa9d7d737
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 23:08:08 +0000

    Thu, Dec 29, 2022, 11:08 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -68,6 +68,7 @@ function Login() {
       navigate('/')
     }
     else if (result === "fail") {
+      flash()
       setFormErrors({
         username: "Invalid username or password",
         password: "Invalid username or password"
