10b5c100c7accd40b2797f07cd63ffd0df7c10c2
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 23:21:38 +0000

    Thu, Dec 29, 2022, 11:21 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -23,9 +23,13 @@ Session(app)
 db = SQL("sqlite:///database.db")
 
 
-# app.config["SESSION_PERMANENT"] = False
-# SESSION_COOKIE_SECURE
-
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/profile", methods=["get"])
 @token_auth.login_required
