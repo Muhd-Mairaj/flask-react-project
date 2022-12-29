9c08a6cafbad7fd9b991cff2f473cedb89f54c89
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 22:22:10 +0000

    Thu, Dec 29, 2022, 10:22 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ import Spinner from 'react-bootstrap/Spinner';
 
 import Body from '../components/Body';
 import InputField from '../components/InputField';
-import { useApi } from '../conte'
+import { useApi } from '../contexts/ApiProvider';
 import '../index.css';
 
 function Home() {
