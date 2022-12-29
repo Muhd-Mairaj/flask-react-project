0068e87c987e95410abca632b4674134325161f2
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 21:06:55 +0000

    Thu, Dec 29, 2022, 9:06 PM +00:00

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Logout from './pages/Logout';
 
 import FlashProvider from './contexts/FlashProvider';
 import UserProvider from './contexts/UserProvider';
-import test_ from './components/TestingSomething';
+import TestingSomething from './components/TestingSomething';
 // import { Auth0Provider } from "@auth0/auth0-react";
 
 function App() {
--- src/components/TestingSomething.js
+++ src/components/TestingSomething.js
@@ -1,6 +1,6 @@
 import { useUser } from '../contexts/UserProvider';
 
-export default function test_({ children }) {
+export default function TestingSomething({ children }) {
   const { user } = useUser()
 
   console.log(user)
