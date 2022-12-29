a058ae282aaaa84651626c67ad8f126f855daf04
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 22:06:03 +0000

    Thu, Dec 29, 2022, 10:06 PM +00:00

--- src/components/PrivateComponent.js
+++ src/components/PrivateComponent.js
@@ -1,9 +1,6 @@
 import { useUser } from '../contexts/UserProvider';
 
 
-// export default function TestingSomething({ children }) {
-//   const { user } = useUser();
-
 //   console.log(user)
   
 // }
