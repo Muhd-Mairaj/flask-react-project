e6d91851fe8da32dda00f11f94c8a3c45e19b32f
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 23:43:31 +0000

    Thu, Dec 29, 2022, 11:43 PM +00:00

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
                   } />
                   <Route path="*" element={
                     <PrivateComponent>
-                      <Route
+                      <Route path="/"
                     </PrivateComponent>
                   }/>
                   <Route path="/" element={<PrivateComponent><Home/></PrivateComponent>} />
