77125643039587b2f627e75e063e08882f187434
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 23:21:43 +0000

    Thu, Dec 29, 2022, 11:21 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -31,6 +31,7 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 @app.route("/profile", methods=["get"])
 @token_auth.login_required
 def profile():
