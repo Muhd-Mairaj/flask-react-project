e3cb2388952cf74299f4d9366484809fce64689e
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 08:52:51 +0000

    Thu, Dec 29, 2022, 8:52 AM +00:00

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function Header({ loggedIn }) {
 
   useEffect(() => {
     if (localStorage.getItem("access_token") !== null) {
-      setIsAuthenticated(flase)
+      setIsAuthenticated(true)
     }
   }, [localStorage.getItem("access_token")])
   
