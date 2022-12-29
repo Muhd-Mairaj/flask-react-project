96b64f90e48da78276533c8a6dd95cd5d37a5cab
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 07:56:54 +0000

    Thu, Dec 29, 2022, 7:56 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -13,6 +13,7 @@ app.config["SECRET_KEY"] = "192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
+
 Session(app)
 
 basic_auth = HTTPBasicAuth()
