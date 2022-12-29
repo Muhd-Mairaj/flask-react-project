dcc6a66815c94b863a0ebe239d2de998439bf2f7
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 22:54:40 +0000

    Thu, Dec 29, 2022, 10:54 PM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -51,7 +51,8 @@ function Login() {
     }
     else {
       setFormErrors({
-        
+        username: "Invalid username or password"
+        username: "Invalid username or password"
       })
     }
     const response = await fetch("/tokens", {
