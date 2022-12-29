c7c47b2da0076577c845e58ef278a4c77ccdc35b
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 23:16:01 +0000

    Thu, Dec 29, 2022, 11:16 PM +00:00

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
           <FlashProvider>
             <ApiProvider>
-              <UserProvider>
+              {/* <UserProvider> */}
                 <Header loggedIn/>
 
                   {/* <PrivateComponent> */}
