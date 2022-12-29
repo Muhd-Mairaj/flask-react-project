a1af94175dfd792633ad7031e0e22ca0637b7179
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 08:08:23 +0000

    Thu, Dec 29, 2022, 8:08 AM +00:00

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -4,11 +4,10 @@ import { useNavigate } from 'react-router-dom';
 
 function Logout() {
   const navigate = useNavigate()
-    localStorage.removeItem('access_token');
-    navigate("/login")
   
   useEffect(() => {
-
+    localStorage.removeItem('access_token');
+    navigate("/login")
   }, []);
 
 }
