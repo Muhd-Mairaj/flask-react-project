ac2087f09d5a30d77e4407e353cfb40c7dca4eb2
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 06:00:52 +0000

    Thu, Dec 29, 2022, 6:00 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -7,7 +7,13 @@ from cs50 import SQL
 
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 basic_auth = HTTPBasicAuth()
+
 db = SQL("sqlite:///database.db")
 
 
