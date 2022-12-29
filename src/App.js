f9e5078fb9026c0aa881c559b93a81a194ea4d44
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-29 23:28:43 +0000

    Thu, Dec 29, 2022, 11:28 PM +00:00

--- src/App.js
+++ src/App.js
@@ -37,9 +37,9 @@ function App() {
                   <Routes>
                     <Route path="/" element={<Home/>}></Route>
                     <Route path="/test" element={<Test/>}></Route>
-                    <Route path="/login" element={<Login/>}></Route>
-                    <Route path="/register" element={<Register/>}></Route>
-                    <Route path="/logout" element={<Logout/>}></Route>
+                    <Route path="/login" element={<P<Login/>}></Route>
+                    <Route path="/register" element={<P<Register/>}></Route>
+                    <Route path="/logout" element={<P<Logout/>}></Route>
                   </Routes>
                   </PrivateComponent>
 
