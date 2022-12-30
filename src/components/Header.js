b993f2510e94b0027a79a204c53b157f52b195b8
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-30 00:27:27 +0000

    Fri, Dec 30, 2022, 12:27 AM +00:00

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ function Header({ loggedIn }) {
   const [ isLoggedIn, setIsLoggedIn ] = useState(false)
 
   useEffect(() => {
-    if (api.isAuthenticated) {
+    if (api.isAuthenticated()) {
       setIsLoggedIn(true)
     }
     else {
