a2eb4561b0bcd01b2e4ccd39c8f4b743088b1277
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-30 00:02:00 +0000

    Fri, Dec 30, 2022, 12:02 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -5,7 +5,36 @@ from secrets import token_urlsafe
 from werkzeug.security import check_password_hash, generate_password_hash
 from cs50 import SQL
 
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 
+
+basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
+
+
+@basic_auth.verify_password
+def verify_password(username, password):
+  rows = db.execute("SELECT id, username, hash FROM users WHERE username = ?", username)
+
+  if len(rows) == 1 and check_password_hash(rows[0]["hash"], password):
+    session["user_id"] = rows[0]["id"]
+    return session["user_id"]
+
+
+@basic_auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403, {'WWW-Authenticate': 'Form'})
+
+
+@token_auth.verify_token
+def verify_token(token):
+  if token == session.get("access_token"):
+    return session["user_id"]
+
+
+@token_auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
 # configure app
 app = Flask(__name__)
 
