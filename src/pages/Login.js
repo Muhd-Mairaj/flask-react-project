c007414d299e7ec9ce6e4ce96d35b0c3f601217b
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-30 05:09:46 +0000

    Fri, Dec 30, 2022, 5:09 AM +00:00

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -50,7 +50,6 @@ function Login() {
     const result = await login(username, password)
     if (result === "ok") {
       flash('You have successfully logged in!', 'success')
-      // setIsLoggedIn(true)
       if (location.state && location.state.next) {
         navigate(location.state.next)
       } 
