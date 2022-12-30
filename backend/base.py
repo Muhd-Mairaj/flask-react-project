39a54d28930592e944d0b3b4a15e2f27d6983c21
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-30 00:02:13 +0000

    Fri, Dec 30, 2022, 12:02 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -28,6 +28,32 @@ Session(app)
 db = SQL("sqlite:///database.db")
 
 
+
+@basic_auth.verify_password
+def verify_password(username, password):
+  rows = db.execute("SELECT id, username, hash FROM users WHERE username = ?", username)
+
+  if len(rows) == 1 and check_password_hash(rows[0]["hash"], password):
+    session["user_id"] = rows[0]["id"]
+    return session["user_id"]
+
+
+@basic_auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403, {'WWW-Authenticate': 'Form'})
+
+
+@token_auth.verify_token
+def verify_token(token):
+  if token == session.get("access_token"):
+    return session["user_id"]
+
+
+@token_auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
