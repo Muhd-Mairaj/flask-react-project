465466578f58a187650c0daa23a07beff652a36b
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-30 04:24:58 +0000

    Fri, Dec 30, 2022, 4:24 AM +00:00

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -70,7 +70,7 @@ export default class APIClient {
   async logout() {
     await this.delete('/logout', {
       headers: {
-        au
+        Authorization: ""
       }
     });
     localStorage.removeItem('access_token');
