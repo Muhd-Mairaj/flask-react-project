110eca702bffb3cd7edc31073f69f5eb7c20e957
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-30 04:13:53 +0000

    Fri, Dec 30, 2022, 4:13 AM +00:00

--- src/contexts/UserProvider.js
+++ src/contexts/UserProvider.js
@@ -20,7 +20,7 @@ export default function UserProvider({ children }) {
   }, [api])
   
   const login = (async (username, password) => {
-    const response = await api.login()
+    const result = await api.login()
   })
   
   return (
