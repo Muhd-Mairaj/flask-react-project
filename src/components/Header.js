8c6af732699d92a17227c0818f44262251324062
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-30 01:15:21 +0000

    Fri, Dec 30, 2022, 1:15 AM +00:00

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ function Header({ loggedIn }) {
     //   </Container>
     // </Navbar>
 
-    {isLoggedIn === undefined ?
+    {isLoggedIn === undefined ? <div></div>
      :
       <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark" className="Header">
         <Container fluid>
