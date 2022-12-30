44e7750b3a281ef186cdb4be631500b1866eb5a9
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-30 00:16:43 +0000

    Fri, Dec 30, 2022, 12:16 AM +00:00

--- src/components/PrivateComponent.js
+++ src/components/PrivateComponent.js
@@ -2,13 +2,6 @@ import { useLocation, useNavigate, Navigate } from 'react-router-dom';
 import { useApi } from '../contexts/ApiProvider';
 
 
-//   console.log(user)
-  
-// }
-
-// import { useLocation, Navigate } from 'react-router-dom';
-// import { useUser } from '../contexts/UserProvider';
-
 export default function PrivateComponent({ children }) {
   const api = useApi()
   const location = useLocation()
