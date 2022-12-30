ff018e345c29aae32ffd565fa168a9e410e29865
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-30 04:51:19 +0000

    Fri, Dec 30, 2022, 4:51 AM +00:00

--- src/components/PublicComponent.js
+++ src/components/PublicComponent.js
@@ -3,7 +3,7 @@ import { useApi } from '../contexts/ApiProvider';
 import { useUser } from '../contexts/UserProvider';
 
 
-export default function PrivateComponent({ children }) {
+export default function PublicComponent({ children }) {
   const api = useApi()
   const isLoggedIn = useUser()
   const location = useLocation()
