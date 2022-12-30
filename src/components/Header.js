61238e5b0b63fb2f53b98dea8ad6335aad0f8ef7
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-30 01:12:07 +0000

    Fri, Dec 30, 2022, 1:12 AM +00:00

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ import '../index.css'
 
 function Header({ loggedIn }) {
   const api = useApi()
-  const [ isLoggedIn, setIsLoggedIn ] = useState(false)
+  const [ isLoggedIn, setIsLoggedIn ] = useState()
 
   useEffect(() => {
     (() => {
