8e2ffe80609f23bcd8de83eb4899f155fed2c745
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-30 04:43:32 +0000

    Fri, Dec 30, 2022, 4:43 AM +00:00

--- src/components/PrivateComponent.js
+++ src/components/PrivateComponent.js
@@ -9,7 +9,7 @@ export default function PrivateComponent({ children }) {
   const location = useLocation()
   const navigate = useNavigate()
   
-  i
+  if 
   if (api.isAuthenticated()) {
     return children
   }
