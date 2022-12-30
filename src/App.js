df14547e5a937ffe8b51c13a9ae9c9709fa242b0
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-30 05:15:37 +0000

    Fri, Dec 30, 2022, 5:15 AM +00:00

--- src/App.js
+++ src/App.js
@@ -45,7 +45,6 @@ function App() {
                         <Routes>
                           <Route path="/" element={<Home/>} />
                           <Route path="/test" element={<Test/>} />
-                          <Route path="/logout" element={<Logout/>} />
                           <Route path="*" element={<Home/>} />
                         </Routes>
                       </PrivateRoute>
