a627241b2b14bcc83aaea57c55700aa05568a566
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 19:47:11 +0000

    Sat, Dec 31, 2022, 7:47 PM +00:00

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,7 +32,7 @@ function Header() {
                   <Navbar.Text className="small">
                     Signed in as: 
                   </Navbar.Text>
-                  <NavDropdown title="Dropdown" variant="dark" size="sm" align="end" id="collapsible-nav-dropdown">
+                  <NavDropdown title="Dropdown" variant="dark" menuVariant="dark" size="sm" align="end" id="collapsible-nav-dropdown">
                     <NavDropdown.Item>Change Password</NavDropdown.Item>
                     <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
                     <NavDropdown.Divider />
