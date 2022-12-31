0f4ed74fefd5399d28c11ec33e1c510553547d4e
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 04:39:04 +0000

    Sat, Dec 31, 2022, 4:39 AM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,8 @@ function Home() {
   
   
   useEffect(() => {
-    
+    const current = new Date();
+  const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`
     (async () => {
       const response = await api.get("/profile", null, {
         headers: {
