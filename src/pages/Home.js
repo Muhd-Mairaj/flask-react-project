2bb5ea160c81f2f1e381f121d18b18b547dc193f
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 04:41:51 +0000

    Sat, Dec 31, 2022, 4:41 AM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -109,7 +109,7 @@ function Home() {
           </thead>
           <tbody>
             {items.map((item, _) => (
-              <tr key={item.key} variant={(Date(item.expiry) < current) ? }>
+              <tr key={item.key} variant={(Date(item.expiry) < current) ? "danger"}>
                 <td>{item.key}</td>
                 <td>{item.item}</td>
                 <td>{item.expiry}</td>
