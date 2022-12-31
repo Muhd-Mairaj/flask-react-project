303d7a2ff13ef079721d7e285e70cf7720952722
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 20:13:59 +0000

    Sat, Dec 31, 2022, 8:13 PM +00:00

--- backend/app.py
+++ backend/app.py
@@ -45,7 +45,7 @@ def unauthorized():
 @token_auth.verify_token
 def verify_token(token):
   if "access_token" in session and token == session["access_token"]:
-    return session["user_id"]
+    return session["user"]
 
 
 @token_auth.error_handler
