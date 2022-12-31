e696f29f122032bcba73f72e26dd9d63ed4800ba
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 06:31:46 +0000

    Sat, Dec 31, 2022, 6:31 AM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -118,7 +118,7 @@ function Home() {
           </thead>
           <tbody>
             {items.map((item, _) => (
-              <tr key={item.key} style={{"background-color": `${item.bg} }}>
+              <tr key={item.key} style={{"background-color": `${item.bg}`}}>
                 <td>{item.key}</td>
                 <td>{item.item}</td>
                 <td>{item.expiry}</td>
