77548d67bf49d41ca96d1077036b91e219d6fbd3
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 14:03:31 +0000

    Sat, Dec 31, 2022, 2:03 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -41,7 +41,7 @@ export default function Home() {
   }
 
   useEffect(() => {
-    useCallbacl(updateItems())
+    useCallback(updateItems())
     console.log("items ", items)
   }, [api, items, updateItems])
 
