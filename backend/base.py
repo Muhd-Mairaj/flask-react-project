a098126b7bec053f75bda7121713b815b715395f
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 11:48:33 +0000

    Sat, Dec 31, 2022, 11:48 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -139,7 +139,7 @@ def register():
   # add user to db
   db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, password_hash)
 
-  return {}, 200
+  return {}, 204
 
 
 @app.route("/login", methods=["POST"])
