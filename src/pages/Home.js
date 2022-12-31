ad1e57010f81b13e4ff0c085fcb8da9c6962d205
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 14:29:08 +0000

    Sat, Dec 31, 2022, 2:29 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -59,7 +59,7 @@ export default function Home() {
       }
     })()
     console.log("items ", items)
-  }, [api, items])
+  }, [api])
 
   async function handleSubmit(event) {
     event.preventDefault();
