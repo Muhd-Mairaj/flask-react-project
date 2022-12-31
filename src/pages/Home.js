ad324c4f9e69ec1c0937f16498c48fe290b37f5b
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 14:20:34 +0000

    Sat, Dec 31, 2022, 2:20 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,23 @@ import InputField from '../components/InputField';
 import { useApi } from '../contexts/ApiProvider';
 import '../index.css';
 
+const updateItems = async () => {
+  const response = await api.get("/profile", null, {
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("access_token")
+    }
+  })
+
+  if (response.ok) {
+    setItems(response.body.items)
+    console.log("updated items to: ", response.body.items)
+    console.log("items now: ", items)
+    // console.log(items)
+  }
+  else {
+    console.log("error: ", response.body)
+  }
+}
 
 export default function Home() {
   const api = useApi()
@@ -22,7 +39,6 @@ export default function Home() {
   const current = new Date();
   const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`
 
-  const updateItems 
 
   useEffect(() => {
     console.log("items ", items)
