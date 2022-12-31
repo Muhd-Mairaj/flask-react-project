a371d4a9ed7a8a081428b00d9a776845d3c3cc83
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 04:27:23 +0000

    Sat, Dec 31, 2022, 4:27 AM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -49,7 +49,7 @@ function Home() {
     date = `${next.getFullYear()}-${next.getMonth()+1}-${next.getDate()}`
     console.log(date)
 
-    if 
+    if ()
 
     event.preventDefault();
     const item = itemField.current.value;
