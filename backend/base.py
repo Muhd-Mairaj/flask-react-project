5b3f3a1f3fd2e66e49c0e4337104b86e9b6e146b
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 16:16:34 +0000

    Sat, Dec 31, 2022, 4:16 PM +00:00

--- backend/base.py
+++ backend/base.py
@@ -98,7 +98,7 @@ def add():
 
     # add item to db
     db.execute("INSERT INTO items (user_id, item, expiry) VALUES(?, ?, ?)", session["user_id"], item, expiry)
-    session[""]
+    session["key_id"] = 
     item = {
       "item": item,
       "expiry": expiry,
