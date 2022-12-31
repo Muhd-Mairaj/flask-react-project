ceea4db8f7e276074c136b226bbd1af5560fe77e
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 14:11:29 +0000

    Sat, Dec 31, 2022, 2:11 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ export default function Home() {
   const current = new Date();
   const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`
 
-  useEffect(() => (async () => {
+  useEffect(() => {(async () => {
     return
   })
     console.log("items ", items)
