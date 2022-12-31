29a6b3b3e94a2e74abefa36d3d7dd0a7c855dd87
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 20:33:19 +0000

    Sat, Dec 31, 2022, 8:33 PM +00:00

--- src/contexts/UserProvider.js
+++ src/contexts/UserProvider.js
@@ -28,7 +28,7 @@ export default function UserProvider({ children }) {
       const response = api.get("/user", null, {
         headers: {Authorization: "Bearer " + localStorage.getItem("access_token")}
       })
-      console.log(respon)
+      console.log(response.body)
       setUser(response.ok ? response.body : null)
       console.log("@#$%^YUHVCXSER%YUHVCDSW#$%^&U ", user)
     }
