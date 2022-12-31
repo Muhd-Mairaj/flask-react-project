0903698ea6f20f43087caa4c9cf1ff292cfb8cad
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 05:03:34 +0000

    Sat, Dec 31, 2022, 5:03 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -71,7 +71,7 @@ def profile():
     items = db.execute("SELECT item, expiry FROM test WHERE id=?", user_id)
 
     for i, item in enumerate(items, 1):
-      expiry_date = datetime.no
+      expiry_date = datetime.strptime("#Y-#")
       item["key"] = i
       item["bg"] = "red" if 1 else ""
 
