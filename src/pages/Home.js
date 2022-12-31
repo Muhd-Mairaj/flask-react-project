d763db954ae2b90cf04de4ee6bbee7b2b1b7a0a1
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 14:04:11 +0000

    Sat, Dec 31, 2022, 2:04 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ export default function Home() {
   const current = new Date();
   const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`
 
-  use
+  useCallback()
   async function updateItems() {
     const response = await api.get("/profile", null, {
       headers: {
