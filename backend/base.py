d52bf7a7ec2f4306f462f34d7ed808d3f3cc84f0
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 11:45:29 +0000

    Sat, Dec 31, 2022, 11:45 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -117,7 +117,7 @@ def register():
     errors["confirm"] = "This field must be filled"
     code = 401
 
-  # return errors if a
+  # return errors before checking validity
   if code != 200:
     return errors, code
 
