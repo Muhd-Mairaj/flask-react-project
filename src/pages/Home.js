47fdf6809738e4d9ca49a4bd69bc90307bccc3eb
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 04:53:47 +0000

    Sat, Dec 31, 2022, 4:53 AM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -109,7 +109,6 @@ function Home() {
           </thead>
           <tbody>
             {items.map((item, _) => (
-              {let }
               <tr key={item.key}>
                 <td>{item.key}</td>
                 <td>{item.item}</td>
