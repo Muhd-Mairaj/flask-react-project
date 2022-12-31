1acefc20a613ce9090efb2beb8340ef19f042dd8
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 14:07:29 +0000

    Sat, Dec 31, 2022, 2:07 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,24 +22,6 @@ export default function Home() {
   const current = new Date();
   const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`
 
-  async function updateItems() {
-    const response = await api.get("/profile", null, {
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("access_token")
-      }
-    })
-
-    if (response.ok) {
-      setItems(response.body.items)
-      console.log("updated items to: ", response.body.items)
-      console.log("items now: ", items)
-      // console.log(items)
-    }
-    else {
-      console.log("error: ", response.body)
-    }
-  }
-
   useEffect(() => {
     updateItems()
     console.log("items ", items)
