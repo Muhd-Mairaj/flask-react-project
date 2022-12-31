3548f1f573d8c822e36940e8b1f88ced9ece56d7
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 11:46:41 +0000

    Sat, Dec 31, 2022, 11:46 AM +00:00

--- backend/base.py
+++ backend/base.py
@@ -140,10 +140,10 @@ def register():
 
   #
   ## Register user  
-    # generate password hash
-    password_hash = generate_password_hash(password)
-    # add user to db
-    db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, password_hash)
+  # generate password hash
+  password_hash = generate_password_hash(password)
+  # add user to db
+  db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, password_hash)
 
 
   return errors, code
