269b0277fbdffd8c2053faaba59f9b75904ed737
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2022-12-31 14:05:19 +0000

    Sat, Dec 31, 2022, 2:05 PM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,26 +22,24 @@ export default function Home() {
   const current = new Date();
   const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`
 
-  useCallback(
+  useCallback()
+  async function updateItems() {
+    const response = await api.get("/profile", null, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("access_token")
+      }
+    })
 
-    async function updateItems() {
-      const response = await api.get("/profile", null, {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("access_token")
-        }
-      })
-  
-      if (response.ok) {
-        setItems(response.body.items)
-        console.log("updated items to: ", response.body.items)
-        console.log("items now: ", items)
-        // console.log(items)
-      }
-      else {
-        console.log("error: ", response.body)
-      }
+    if (response.ok) {
+      setItems(response.body.items)
+      console.log("updated items to: ", response.body.items)
+      console.log("items now: ", items)
+      // console.log(items)
     }
-  )
+    else {
+      console.log("error: ", response.body)
+    }
+  }
 
   useEffect(() => {
     updateItems()
