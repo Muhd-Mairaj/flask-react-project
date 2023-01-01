25cb47dee2a6df2255b17ccde316a1f78cc65c2f
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-01-01 02:38:25 +0000

    Sun, Jan 1, 2022, 2:38 AM +00:00

--- backend/api.py
+++ backend/api.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 # Make sure SECRET KEY is set
 if not os.environ.get("SECRET_KEY"):
-    raise RuntimeError("API_KEY not set")
+    raise RuntimeError("SECRET_KEY not set. ")
 
 
 # configure secret_key
