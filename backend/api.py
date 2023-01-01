051db9a72f2cc8b50dd937ad796500e824c53b05
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-01-01 02:38:40 +0000

    Sun, Jan 1, 2022, 2:38 AM +00:00

--- backend/api.py
+++ backend/api.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 # Make sure SECRET KEY is set
 if not os.environ.get("SECRET_KEY"):
-    raise RuntimeError("SECRET_KEY not set. Run 'export ")
+    raise RuntimeError("SECRET_KEY not set. Run 'export SECRET_KEY=")
 
 
 # configure secret_key
