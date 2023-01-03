8fcde7081232138fee18d04fcef1cd5084c285ad
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-01-03 05:10:33 +0000

    Tue, Jan 3, 2023, 5:10 AM +00:00

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -62,7 +62,7 @@ function Register() {
     console.log("response.body ", response.body)
     if (!response.ok) {
       setFormErrors(response.body)
-      usernameField
+      
       return
     }
     else {
