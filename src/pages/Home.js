30ad1782f3dc9b5f79303fec23c1c207538d033c
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-01-04 05:18:27 +0000

    Wed, Jan 4, 2023, 5:18 AM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,7 +20,7 @@ export default function Home() {
   const expiryField = useRef()
 
   const current = new Date();
-  const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`
+  // const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`
   const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`
   console.log("date", date)
 
