31ae101eb2cb057c56172191c3e0acfe2e3d53a8
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-01-04 04:37:16 +0000

    Wed, Jan 4, 2023, 4:37 AM +00:00

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -8,7 +8,7 @@ function InputField(
       {label && <Form.Label className={class_name}>{label}</Form.Label>}
       <Form.Control
         type={type || 'text'}
-        min={min || 0}
+        min={min || }
         placeholder={placeholder}
         ref={fieldRef}
         className={class_name}
