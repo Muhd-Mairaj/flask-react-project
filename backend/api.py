65c4e7dd0d44a3dd9de75c6c8448728a7469b2ea
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-01-04 04:21:46 +0000

    Wed, Jan 4, 2023, 4:21 AM +00:00

--- backend/api.py
+++ backend/api.py
@@ -165,7 +165,7 @@ def register():
   # check password strength
   if not check_password_strength(password):
     errors["password"] = "Password must be 8 characters, including upper, lower, digits and symbols (!&()*,-./;=?@[\]_{|})"
-    errors["confirm"] = "Password must be 8 characters, including upper, lower, digits and symbols (!&()*,-./;=?@[\]_{|})"
+    errors["confirm"] = "Password must be 8 characters, including upper, lower, digits and symbols (!&()*,-./;=?@[\\]_{|})"
     return errors, 401
 
   #
