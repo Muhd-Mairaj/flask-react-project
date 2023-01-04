06f0e70f48d09b08720e674365e28200d36ad31f
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-01-04 05:18:53 +0000

    Wed, Jan 4, 2023, 5:18 AM +00:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,7 @@ export default function Home() {
 
   const current = new Date();
   // const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`
-  const date = current.getFullYear + "-" +  `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`
+  const date = current.getFullYear + "-" +  -${current.getMonth()+1}-${current.getDate()}`
   console.log("date", date)
 
   useEffect(() => {
