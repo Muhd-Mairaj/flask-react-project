1980c2ad9fda39b26b4693b4a3c4fe454b6958a0
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-06 06:51:36 +0000

    Mon, Mar 6, 2023, 9:51 AM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -118,6 +118,7 @@ export default function Home() {
             <tr>
               <th>Item</th>
               <th>Expiry</th>
+              <th
             </tr>
           </thead>
           <tbody>
