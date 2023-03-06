e5eeb8c78594eac1a68313b48cf6b4fb2cefe62b
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-06 08:29:04 +0000

    Mon, Mar 6, 2023, 11:29 AM +03:00

--- db.py
+++ db.py
@@ -26,4 +26,5 @@ db.execute("CREATE TABLE items (user_id INTEGER, item_id INTEGER, item TEXT NOT 
 
 for row in items:
   db.execute("INSERT INTO items (user_id, item_id, item, expiry) VALUES(?, ?, ?, ?)",
-              row["user_id"])
+              row["user_id"],
+              row["item_id"])
