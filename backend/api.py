1181a9d8e16133b18fc502c2b4846aa33a9b9b78
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-11 19:12:51 +0000

    Sat, Mar 11, 2023, 10:12 PM +03:00

--- backend/api.py
+++ backend/api.py
@@ -125,7 +125,7 @@ def add():
         return errors, 400
 
     # add item to db
-    
+    print()
 
     db.execute("INSERT INTO items (user_id, item, expiry) VALUES(?, ?, ?)", session["user"]["id"], item, expiry)
 
