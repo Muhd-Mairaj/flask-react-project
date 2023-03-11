ed33c860b63363bc9fc1c4d1269cae2a8e284af8
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-11 19:06:24 +0000

    Sat, Mar 11, 2023, 10:06 PM +03:00

--- backend/api.py
+++ backend/api.py
@@ -142,6 +142,9 @@ def add():
 @app.route("/profile", methods=["DELETE"])
 @token_auth.login_required
 def remove():
+    key = request.headers.get('id')
+    print(f"{key = }")
+
     if not key:
         return jsonify("no id included"), 400
 
