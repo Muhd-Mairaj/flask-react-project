4f051808a644a826f4cccb975e8de2225019ea48
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-11 22:16:04 +0000

    Sun, Mar 12, 2023, 1:16 AM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -100,11 +100,6 @@ export default function Home() {
     }
 
     // remove item from items list
-    console.log(response);
-    console.log("items", items);
-    let array = items.filter(function(item) {return item.item_id !== key})
-    console.log("array", array);
-
     setItems(items.filter(function(item) {return item.item_id !== key}))
 
     // const response =
