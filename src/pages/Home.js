1603bbd5417c0a671f50d639672c2335fd8f558e
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-11 21:58:57 +0000

    Sun, Mar 12, 2023, 12:58 AM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -100,7 +100,7 @@ export default function Home() {
     }
 
     // remove item from items list
-    setItems(items.filter())
+    setItems(items.filter(()))
     console.log("SUCCESS");
     console.log(response);
 
