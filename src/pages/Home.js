3b4181c06c07a4f806916efbb14c7cb03bc9eb3a
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-11 18:18:03 +0000

    Sat, Mar 11, 2023, 9:18 PM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -95,7 +95,7 @@ export default function Home() {
     if (!response.ok) {
       // do something
       console.log("FAILED");
-      console.log(response.e)
+      console.log(response)
       return
     }
 
