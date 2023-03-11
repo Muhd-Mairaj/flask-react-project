f70d8d058e3e8a677a9c6845740c03a0006dbb16
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-11 21:02:05 +0000

    Sun, Mar 12, 2023, 12:02 AM +03:00

--- backend/api.py
+++ backend/api.py
@@ -134,7 +134,7 @@ def add():
     item_query = db.execute("SELECT * FROM item WHERE user_id = ?", session["user"]["id"])
 
     if len(item_query) != 1:
-        return jsonify("Something went wrong")
+        return jsonify("Something went wrong"), 500
     item = item_query[0]
     # item = {
     #     "item": item,
