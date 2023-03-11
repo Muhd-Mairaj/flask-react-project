11c8bb5389cc6e86606d18cd8ab41bd839a48584
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-11 21:46:34 +0000

    Sun, Mar 12, 2023, 12:46 AM +03:00

--- backend/api.py
+++ backend/api.py
@@ -161,6 +161,7 @@ def remove():
 
     # remove item from database
     # db.execute("DELETE FROM items WHERE user_id = ? AND item_id = ?", session["user"]["id"], key)
+    print(removing thing from database)
 
 
 
