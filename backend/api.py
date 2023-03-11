e17cc66b08b6ee73675d7d5fb229620517ea73b7
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-11 17:22:43 +0000

    Sat, Mar 11, 2023, 8:22 PM +03:00

--- backend/api.py
+++ backend/api.py
@@ -151,13 +151,13 @@ def remove():
 
     query = db.execute("SELECT * FROM items WHERE user_id = ?", session["user"]["id"])
 
-    for item in query:
+    for i, item in items:
         if key == item["item_id"]:
             # remove from database
             db.execute("DELETE FROM items WHERE item_id = ?", key)
 
             # remove from items list
-            del session["items"][key]
+            del session["items"][i]
 
             return {}, 204 # succesful, no response body
 
