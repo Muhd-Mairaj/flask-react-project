49a7fc47ea0d8547f39b4c1cfc8a70b567ff93a3
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 14:24:39 +0000

    Mon, Mar 13, 2023, 5:24 PM +03:00

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -11,7 +11,7 @@ function TableRow({ editing, key, style, item, expiry, onRemove, onEdit }) {
 
   return (
     <>
-      {editing && key}
+      {key && key}
       {(editing === key) ?
           <tr key={key}>
             <td>
