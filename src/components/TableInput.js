4db2a3546a7441ef51ed8e2c948d156250f865f2
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 17:13:16 +0000

    Mon, Mar 13, 2023, 8:13 PM +03:00

--- src/components/TableInput.js
+++ src/components/TableInput.js
@@ -12,6 +12,7 @@ function TableRow({ key, item, itemFieldRef, expiry, expiryFieldRef, onCancel, o
     console.log("ref.current.value", ref.current.value)
 
     const updatedText = event.target.value;
+    console.log("ref.current.value", ref.current.value)
 
     ref.current.value = updatedText;
     return
