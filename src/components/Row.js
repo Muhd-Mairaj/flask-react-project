8cacbaa17e0a24d181c4e01450f1a8d078a9c06c
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 09:42:10 +0000

    Mon, Mar 13, 2023, 12:42 PM +03:00

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,6 @@
 import { InputField } from './InputField';
 
-function Row({ editing, key, style, item, expir }) {
+function Row({ editing, key, style, item, expiry, }) {
   return (
     {editing && InputField}
   )
