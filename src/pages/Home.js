d7e19eeb6b72310cb05b452a68b634c2c44911ba
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 09:51:44 +0000

    Mon, Mar 13, 2023, 12:51 PM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ import Spinner from 'react-bootstrap/Spinner';
 import Body from '../components/Body';
 import InputField from '../components/InputField';
 import { useApi } from '../contexts/ApiProvider';
-import Row from '../components/TableRow';
+import TableRow from '../components/TableRow';
 
 import '../index.css';
 
