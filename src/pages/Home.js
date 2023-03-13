ddfbde39c69fbbb4c2e3011b84e75f07968aefe4
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 13:08:52 +0000

    Mon, Mar 13, 2023, 4:08 PM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -110,7 +110,9 @@ export default function Home() {
   }
 
   async function editItem(key) {
-    isEditing.map((item, _)) => {}
+    isEditing.map((item, _)) => (
+      
+    )
     setIsEditing([...isEditing, key]);
     return;
 
