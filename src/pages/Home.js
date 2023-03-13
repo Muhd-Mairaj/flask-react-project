4a7b49667dcf0adf0d4205d38e8132a9da12f4b4
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 21:41:00 +0000

    Tue, Mar 14, 2023, 12:41 AM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -164,7 +164,7 @@ export default function Home() {
 
     let array = []
     for (let i of items) {
-      if (i.key === response.body.item_id) {
+      if (i.item_id === response.body.item_id) {
         array.push(response.body)
       }
       else {
