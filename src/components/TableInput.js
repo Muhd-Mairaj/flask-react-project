d7f25bb101b283c844acd1af17d6534d725d2e98
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 22:13:44 +0000

    Tue, Mar 14, 2023, 1:13 AM +03:00

--- src/components/TableInput.js
+++ src/components/TableInput.js
@@ -29,33 +29,33 @@ function TableRow({ key, item, itemFieldRef, itemError, expiry, expiryFieldRef, 
     <>
     <Form onSubmit={onConfirm}>
 
-        <tr key={key}>
-          <td>
-            <InputField
-              name="item"
-              placeholder="Item name"
-              value={itemField}
-              onChange={(event) => {setItemField(event.target.value)}}
-              fieldRef={itemFieldRef}
-              error={itemError}
-              />
-          </td>
-          <td>
-            <InputField
-              name="expiry"
-              placeholder="expiry"
-              type="date"
-              value={expiryField}
-              onChange={(event) => {setExpiryField(event.target.value)}}
-              fieldRef={expiryFieldRef}
-              error={expiryError}
+      <tr key={key}>
+        <td>
+          <InputField
+            name="item"
+            placeholder="Item name"
+            value={itemField}
+            onChange={(event) => {setItemField(event.target.value)}}
+            fieldRef={itemFieldRef}
+            error={itemError}
             />
-          </td>
-          <td>
-            <Button variant="dark" className="w-5" onClick={onCancel}>Cancel</Button>
-            <Button variant="dark" className="w-5 ms-2" onClick={onConfirm}>Confirm</Button>
-          </td>
-        </tr>
+        </td>
+        <td>
+          <InputField
+            name="expiry"
+            placeholder="expiry"
+            type="date"
+            value={expiryField}
+            onChange={(event) => {setExpiryField(event.target.value)}}
+            fieldRef={expiryFieldRef}
+            error={expiryError}
+          />
+        </td>
+        <td>
+          <Button variant="dark" className="w-5" onClick={onCancel}>Cancel</Button>
+          <Button variant="dark" className="w-5 ms-2" onClick={onConfirm}>Confirm</Button>
+        </td>
+      </tr>
     </Form>
     </>
   )
