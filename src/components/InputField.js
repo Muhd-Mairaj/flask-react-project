ce5bc5b9ab45255c6938f5f22b3c38e1a034fc29
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 17:06:26 +0000

    Mon, Mar 13, 2023, 8:06 PM +03:00

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -6,20 +6,20 @@ function InputField({ name, label, type, value, onChange, min, placeholder, erro
     <Form.Group controlId={name} className="InputField">
       {label && <Form.Label className={class_name}>{label}</Form.Label>}
       {(value && onChange) ? <Form.Control
-        type={type || 'text'}
-        min={min || ""}
-        placeholder={placeholder}
-        ref={fieldRef}
-        className={class_name}
-      />
-    :
-      <Form.Control
-        type={type || 'text'}
-        min={min || ""}
-        placeholder={placeholder}
-        ref={fieldRef}
-        className={class_name}
-      />
+          type={type || 'text'}
+          min={min || ""}
+          placeholder={placeholder}
+          ref={fieldRef}
+          className={class_name}
+        />
+      :
+        <Form.Control
+          type={type || 'text'}
+          min={min || ""}
+          placeholder={placeholder}
+          ref={fieldRef}
+          className={class_name}
+        />
     }
       <div className="mx-1">
         <Form.Text className="text-danger text-center">{error}</Form.Text>
