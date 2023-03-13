7e5493c5abe43ef89d6d5699bcb2df1fea0c7704
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 17:07:24 +0000

    Mon, Mar 13, 2023, 8:07 PM +03:00

--- src/components/TableInput.js
+++ src/components/TableInput.js
@@ -15,7 +15,7 @@ function TableRow({ key, item, itemFieldRef, expiry, expiryFieldRef, onCancel, o
             name="item"
             placeholder="Item name"
             value={item}
-            onChange=
+            onChange={()}
             fieldRef={itemFieldRef}
           />
         </td>
