392cc62cdc9cfc55e7422d250b4ec2d33449cf1e
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 13:14:29 +0000

    Mon, Mar 13, 2023, 4:14 PM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -114,7 +114,7 @@ export default function Home() {
     for (let item in isEditing) {
       console.log("item", item);
       if (key === item) {
-        console.log("item", )
+        console.log("item", item, "matches")
         return;
       }
     }
