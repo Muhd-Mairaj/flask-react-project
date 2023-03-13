1d4b2a2bca784f962c291755eec7cbb95d43ef5c
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 14:30:10 +0000

    Mon, Mar 13, 2023, 5:30 PM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -166,7 +166,7 @@ export default function Home() {
           </thead>
           <tbody>
             {items.map((item, _) => (
-              
+              {}
               <TableRow
                 key={item.item_id}
                 style={{"backgroundColor": `${item.bg === "red" ? "#ff0000a0": ""}`}}
