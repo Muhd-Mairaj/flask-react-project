65c320944ad5adaeade20e27bc1a18c3f0418ab6
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 17:59:20 +0000

    Mon, Mar 13, 2023, 8:59 PM +03:00

--- src/components/TableInput.js
+++ src/components/TableInput.js
@@ -5,7 +5,7 @@ import InputField from './InputField';
 
 function TableRow({ key, item, itemFieldRef, expiry, expiryFieldRef, onCancel, onEdit }) {
   const [itemField, setItemField] = useState(item);
-  const [itemField, setItemField] = useState(item);
+  const [expiryField, setexpiryField] = useState(expiry);
 
   useEffect(() => {
   }, [key])
