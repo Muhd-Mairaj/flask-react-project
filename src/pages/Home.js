e2ab1de1b29b5a7f2d539bb1ce31b0fda8b6a0b5
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 14:30:33 +0000

    Mon, Mar 13, 2023, 5:30 PM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -166,7 +166,7 @@ export default function Home() {
           </thead>
           <tbody>
             {items.map((item, _) => (
-              {isEditing === {item.item_id}}
+              {isEditing === {item.item_id} ?}
               <TableRow
                 key={item.item_id}
                 style={{"backgroundColor": `${item.bg === "red" ? "#ff0000a0": ""}`}}
