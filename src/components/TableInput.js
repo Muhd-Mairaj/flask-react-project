59b00632dcd216fa2308e6fc2de547a535fb7780
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 14:31:56 +0000

    Mon, Mar 13, 2023, 5:31 PM +03:00

---
+++ src/components/TableInput.js
@@ -0,0 +1,55 @@
+import { useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import InputField from './InputField';
+
+
+function TableRow({ editing, key, style, item, expiry, onRemove, onEdit }) {
+  useEffect(() => {
+    console.log("editing", editing)
+    console.log("key", key)
+  }, [editing, key])
+
+  return (
+    <>
+      {/* {key && editing === key ?
+          <tr key={key}>
+            <td>
+              <InputField
+                name="item"
+                placeholder="Item name"
+              />
+            </td>
+            <td>
+              <InputField
+                name="expiry"
+                placeholder="expiry"
+              />
+            </td>
+            <td>
+              <Button variant="dark" className="w-5" onClick={onRemove}>Remove</Button>
+              <Button variant="dark" className="w-5 px-4 ms-2" onClick={onEdit}>Edit</Button>
+            </td>
+          </tr>
+        :
+          <tr key={key} style={style}>
+            <td>{item}</td>
+            <td>{expiry}</td>
+            <td>
+              <Button variant="dark" className="w-5" onClick={onRemove}>Remove</Button>
+              <Button variant="dark" className="w-5 px-4 ms-2" onClick={onEdit}>Edit</Button>
+            </td>
+          </tr>
+      } */}
+      <tr key={key} style={style}>
+        <td>{item}</td>
+        <td>{expiry}</td>
+        <td>
+          <Button variant="dark" className="w-5" onClick={onRemove}>Remove</Button>
+          <Button variant="dark" className="w-5 px-4 ms-2" onClick={onEdit}>Edit</Button>
+        </td>
+      </tr>
+    </>
+  )
+}
+
+export default TableRow;
