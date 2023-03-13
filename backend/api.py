740e31980588a1afc801d9c0fbcd80313f911924
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 21:26:18 +0000

    Tue, Mar 14, 2023, 12:26 AM +03:00

--- backend/api.py
+++ backend/api.py
@@ -197,7 +197,7 @@ def edit():
 
 
     # update item
-    db.execute("UPDATE items SET item = ?, expiry = ? where item_id = ?", item, expiry, )
+    db.execute("UPDATE items SET item = ?, expiry = ? where item_id = ?", item, expiry, item_id)
     #UPDATE "items" SET "user_id"='6', "item_id"='13', "item"='item 3', "expiry"='2023-03-22' WHERE "rowid" = 13
 
     # make queries
