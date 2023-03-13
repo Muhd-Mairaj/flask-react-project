c215be120a43d8b4d6c5f3a9b11a1f3517a7b48f
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 12:00:42 +0000

    Mon, Mar 13, 2023, 3:00 PM +03:00

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,8 +1,6 @@
 import Form from 'react-bootstrap/Form';
 
-function InputField(
-  { name, label, type, min, placeholder, error, fieldRef, class_name }
-) {
+function InputField({ name, label, type, min, placeholder, error, fieldRef, class_name }) {
   return (
     <Form.Group controlId={name} className="InputField">
       {label && <Form.Label className={class_name}>{label}</Form.Label>}
