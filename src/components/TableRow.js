6e1e81bb45d51e9728eac59f171251e266f56ade
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 15:28:09 +0000

    Mon, Mar 13, 2023, 6:28 PM +03:00

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -11,35 +11,6 @@ function TableRow({ editing, key, style, item, expiry, onRemove, onEdit }) {
 
   return (
     <>
-      {/* {key && editing === key ?
-          <tr key={key}>
-            <td>
-              <InputField
-                name="item"
-                placeholder="Item name"
-              />
-            </td>
-            <td>
-              <InputField
-                name="expiry"
-                placeholder="expiry"
-              />
-            </td>
-            <td>
-              <Button variant="dark" className="w-5" onClick={onRemove}>Remove</Button>
-              <Button variant="dark" className="w-5 px-4 ms-2" onClick={onEdit}>Edit</Button>
-            </td>
-          </tr>
-        :
-          <tr key={key} style={style}>
-            <td>{item}</td>
-            <td>{expiry}</td>
-            <td>
-              <Button variant="dark" className="w-5" onClick={onRemove}>Remove</Button>
-              <Button variant="dark" className="w-5 px-4 ms-2" onClick={onEdit}>Edit</Button>
-            </td>
-          </tr>
-      } */}
       <tr key={key} style={style}>
         <td>{item}</td>
         <td>{expiry}</td>
