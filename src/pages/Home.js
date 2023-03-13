f4bfa85dba5611acc567a95595ad657a5e068fa1
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 21:51:16 +0000

    Tue, Mar 14, 2023, 12:51 AM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -233,7 +233,7 @@ export default function Home() {
                     key={item.item_id}
                     item={item.item}
                     itemFieldRef={editItemField}
-                    expiryError={editFormErrors.expiry}
+                    itemError={editFormErrors.expiry}
                     expiry={item.expiry}
                     expiryFieldRef={editExpiryField}
                     expiryError={editFormErrors.expiry}
