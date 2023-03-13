19162d32b4de4b24062f781b46629c1741cd9527
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 09:45:15 +0000

    Mon, Mar 13, 2023, 12:45 PM +03:00

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,7 +2,12 @@ import { InputField } from './InputField';
 
 function Row({ editing, key, style, item, expiry, onRemove, onEdit }) {
   return (
-    {editing ? <InputField> }
+    {editing ?             <InputField
+      name="item"
+      placeholder="Item name"
+      fieldRef={itemField}
+      error={formErrors.item}
+    /> }
   )
 }
 
