ce098b5e58510d31dd23c11ad95154a8f3633098
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 17:18:13 +0000

    Mon, Mar 13, 2023, 8:18 PM +03:00

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -4,7 +4,7 @@ import InputField from './InputField';
 
 
 function TableRow({ editing, key, style, item, expiry, onRemove, onEdit }) {
-  co
+  const
 
   useEffect(() => {
     console.log("editing", editing)
