eed3a25f3ff3a990ffe593977ec58adab1910a33
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 21:26:37 +0000

    Tue, Mar 14, 2023, 12:26 AM +03:00

--- backend/api.py
+++ backend/api.py
@@ -199,7 +199,7 @@ def edit():
     # update item
     print("EDITING STUFF IN DATABASE")
     db.execute("UPDATE items SET item = ?, expiry = ? where item_id = ?", item, expiry, item_id)
-
+    
 
 
     return {}, 204
