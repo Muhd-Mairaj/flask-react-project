b8018d0f95da9e40d7026055158c360263736fad
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 11:44:02 +0000

    Mon, Mar 13, 2023, 2:44 PM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -110,7 +110,7 @@ export default function Home() {
   }
 
   async function editItem(key) {
-    setIsEditing()
+    setIsEditing(!)
     return;
 
     const response = await api.put("/profile", {
