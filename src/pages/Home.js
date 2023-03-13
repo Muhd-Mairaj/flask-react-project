c593cbd3082112c6fd839237d63fb519abf06ee7
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 11:43:37 +0000

    Mon, Mar 13, 2023, 2:43 PM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,7 +18,7 @@ export default function Home() {
   const api = useApi()
   const [items, setItems] = useState([])
   const [formErrors, setFormErrors] = useState({})
-  const [isEditing, setIsEditing] = useState(false)
+  const [editing, setIsEditing] = useState(false)
   const itemField = useRef()
   const expiryField = useRef()
 
