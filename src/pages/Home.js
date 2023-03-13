396b2e956a40a54b33da3a112dd38dbcef140873
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 21:43:57 +0000

    Tue, Mar 14, 2023, 12:43 AM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,7 +28,7 @@ export default function Home() {
 
   const editItemField = useRef()
   const editExpiryField = useRef()
-  const [formErrors, setFormErrors] = useState({})
+  const [editormErrors, setFormErrors] = useState({})
 
   function getDate() {
     const current = new Date();
