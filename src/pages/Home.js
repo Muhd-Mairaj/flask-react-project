270dcdd668d64bbae053d2cef68b8d582dfb4c42
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 21:50:26 +0000

    Tue, Mar 14, 2023, 12:50 AM +03:00

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -235,7 +235,7 @@ export default function Home() {
                     itemFieldRef={editItemField}
                     expiry={item.expiry}
                     expiryFieldRef={editExpiryField}
-                    expiry
+                    error=
                     onCancel={cancelEdit}
                     onEdit={() => {makeEdit(item.item_id)}}
                   />
