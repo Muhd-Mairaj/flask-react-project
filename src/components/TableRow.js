3b1b92357e8260a54ff1b90692ddec42cb7bb3a0
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 14:12:52 +0000

    Mon, Mar 13, 2023, 5:12 PM +03:00

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -24,7 +24,7 @@ function TableRow({ editing, key, style, item, expiry, onRemove, onEdit }) {
           </td>
         </tr>
       } */}
-      <tr key={key} style={style}>
+      {/* <tr key={key} style={style}>
       <td>{item}</td>
       <td>{expiry}</td>
       <td>
@@ -32,7 +32,7 @@ function TableRow({ editing, key, style, item, expiry, onRemove, onEdit }) {
         <Button variant="dark" className="w-5 px-4 ms-2" onClick={onEdit}>Edit</Button>
       </td>
     </tr>
-    </>
+    </> */}
   )
 }
 
