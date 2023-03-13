9e5cfb27970daedb0ccb72216898548e5f309415
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-03-13 11:52:38 +0000

    Mon, Mar 13, 2023, 2:52 PM +03:00

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -9,6 +9,42 @@ function TableRow({ editing, key, style, item, expiry, onRemove, onEdit }) {
   }, [editing])
 
   return (
+    {items ? <Table striped bordered responsive="lg" className="Home">
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Expiry</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, _) => (
+              <TableRow
+                key={item.item_id}
+                style={{"backgroundColor": `${item.bg === "red" ? "#ff0000a0": ""}`}}
+                item={item.item}
+                expiry={item.expiry}
+                onRemove={() => {removeItem(item.item_id)}}
+                onEdit={() => {editItem(item.item_id)}}
+                editing={isEditing}
+              />
+              // <tr key={item.item_id} style={{"backgroundColor": `${item.bg === "red" ? "#ff0000a0": ""}`}}>
+              //   <td>{item.item}</td>
+              //   <td>{item.expiry}</td>
+              //   <td>
+              //     <Button variant="dark" className="w-5" onClick={() => {removeItem(item.item_id)}}>Remove</Button>
+              //     <Button variant="dark" className="w-5 px-4 ms-2" onClick={() => {editItem(item.item_id)}}>Edit</Button>
+              //   </td>
+              // </tr>
+            ))}
+          </tbody>
+        </Table>
+      :
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      }
+
     {editing ? <InputField
           name="item"
           placeholder="Item name"
